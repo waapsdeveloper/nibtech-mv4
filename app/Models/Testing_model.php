45e9402df8df4822d03b956f3f2413d9d36d10f7
haleem24 <101307148+haleem24@users.noreply.github.com>
2024-04-26 19:55:44 +0100

    Update and rename testing_model.php to Testing_model.php

--- app/Models/Testing_model.php
+++ app/Models/Testing_model.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class testing_model extends Model
+class Testing_model extends Model
 {
     use HasFactory;
     protected $table = 'testing';
