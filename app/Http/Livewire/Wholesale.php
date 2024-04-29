32528ba1e3b3e65f5e49041915ff5d74fe9c8851
haleem24 <haleem.shah24@gmail.com>
2024-04-29 19:32:00 +0100

    Tes

--- app/Http/Livewire/Wholesale.php
+++ app/Http/Livewire/Wholesale.php
@@ -344,8 +344,8 @@ class Wholesale extends Component
         if(!$name){
             print_r($dh);
             session()->put('error', "Heading not Found(name, imei)");
-            // return redirect()->back();
-            die;
+            return redirect()->back();
+            // die;
         }
         // echo $name;
         $imei = array_search('imei', $arrayLower);
