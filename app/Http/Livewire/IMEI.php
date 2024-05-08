10a5922aff924f2581aa3dbf5c16db259ec15c07
haleem24 <haleem.shah24@gmail.com>
2024-05-08 11:49:15 +0100

    AfterSale

--- app/Http/Livewire/IMEI.php
+++ app/Http/Livewire/IMEI.php
@@ -51,28 +51,46 @@ class IMEI extends Component
                 // return redirect()->back(); // Redirect here is not recommended
                 return view('livewire.imei', $data); // Return the Blade view instance with data
             }
-            $sale_status = Order_item_model::where(['stock_id'=>$stock->id,'linked_id'=>$stock->purchase_item->id])->first();
-            if($stock->status == 1){
-                if($sale_status != null){
-                    $stock->status = 2;
-                    $stock->save();
-                    session()->put('success', 'IMEI Sold');
-                }else{
-                    session()->put('success', 'IMEI Available');
-                }
+            // $sale_status = Order_item_model::where(['stock_id'=>$stock->id,'linked_id'=>$stock->purchase_item->id])->first();
+            // if($stock->status == 1){
+            //     if($sale_status != null){
+            //         $stock->status = 2;
+            //         $stock->save();
+            //         session()->put('success', 'IMEI Sold');
+            //     }else{
+            //         session()->put('success', 'IMEI Available');
+            //     }
+            // }
+            // if($stock->status == 2){
+            //     if($sale_status == null){
+            //         $stock->status = 1;
+            //         $stock->save();
+            //         session()->put('success', 'IMEI Available');
+            //     }else{
+            //         session()->put('success', 'IMEI Sold');
+            //     }
+            // }
+            $last_item = Order_item_model::find($stock->purchase_item->id);
+            while(Order_item_model::where('linked_id',$last_item->id)->first()){
+                $last_item = Order_item_model::where('linked_id',$last_item->id)->first();
             }
-            if($stock->status == 2){
-                if($sale_status == null){
+            if(in_array($last_item->order->order_type_id,[1,4])){
+
+                if($stock->status == 2){
                     $stock->status = 1;
                     $stock->save();
                     session()->put('success', 'IMEI Available');
                 }else{
                     session()->put('success', 'IMEI Sold');
                 }
-            }
-            $last_item = Order_item_model::find($stock->purchase_item->id);
-            while(Order_item_model::where('linked_id',$last_item->id)->first()){
-                $last_item = Order_item_model::where('linked_id',$last_item->id)->first();
+            }else{
+                if($stock->status == 1){
+                    $stock->status = 2;
+                    $stock->save();
+                    session()->put('success', 'IMEI Sold');
+                }else{
+                    session()->put('success', 'IMEI Available');
+                }
             }
             print_r($last_item);
             $stock_id = $stock->id;
