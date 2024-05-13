aba6f65ccfddea2f0b2a959057ac33117d2781cb
haleem24 <haleem.shah24@gmail.com>
2024-05-13 17:20:24 +0100

    AfterSale

--- app/Http/Livewire/RMA.php
+++ app/Http/Livewire/RMA.php
@@ -246,6 +246,12 @@ class RMA extends Component
                     }
                 </script>";
                 exit;
+            }else{
+                $this->add_rma_item($order_id);
+                session()->put('bypass_check', 1);
+                request()->merge(['bypass_check'=> 1]);
+                return redirect()->back();
+
             }
         }
     }
