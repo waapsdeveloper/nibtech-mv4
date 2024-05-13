d9502bf80a82a7744f6dee8c79c1a60640a309cf
haleem24 <haleem.shah24@gmail.com>
2024-05-13 20:54:52 +0100

    AfterSale

--- app/Models/Stock_model.php
+++ app/Models/Stock_model.php
@@ -63,9 +63,12 @@ class Stock_model extends Model
     }
     public function last_item(){
 
-        $last_item = $this->purchase_item()->first();
-        while(Order_item_model::where('linked_id',$last_item->id)->first()){
-            $last_item = Order_item_model::where('linked_id',$last_item->id)->first();
+        $last_item = $this->purchase_item;
+        if($last_item != null){
+
+            while(Order_item_model::where('linked_id',$last_item->id)->first()){
+                $last_item = Order_item_model::where('linked_id',$last_item->id)->first();
+            }
         }
         return $last_item;
     }
