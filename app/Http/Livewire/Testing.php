2aeb78d1d8ba756868174f0b3d365ce627df6656
haleem24 <haleem.shah24@gmail.com>
2024-05-14 20:42:42 +0100

    AfterSale

--- app/Http/Livewire/Testing.php
+++ app/Http/Livewire/Testing.php
@@ -30,7 +30,7 @@ class Testing extends Component
 
         $requests = Api_request_model::where('status',null)->get();
         foreach($requests as $request){
-            $data = json_decode($request->request);
+            $data = $request->request;
             // $stock = Stock_model::where('imei',$data->Imei)->orWhere('imei',$data->Imei2)->orWhere('serial_number',$data->Serial)->first();
         echo "<pre>";
 
