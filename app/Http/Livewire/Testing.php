be420fe5b948fd2502dbd5f21cb56191b351d203
haleem24 <haleem.shah24@gmail.com>
2024-05-15 10:08:01 +0100

    AfterSale

--- app/Http/Livewire/Testing.php
+++ app/Http/Livewire/Testing.php
@@ -32,11 +32,11 @@ class Testing extends Component
         foreach($requests as $request){
             $data = $request->request;
             $datas = json_decode(json_decode(preg_split('/(?<=\}),(?=\{)/', $data)[0]));
-            // $stock = Stock_model::where('imei',$data->Imei)->orWhere('imei',$data->Imei2)->orWhere('serial_number',$data->Serial)->first();
+            $stock = Stock_model::where('imei',$datas->Imei)->orWhere('imei',$datas->Imei2)->orWhere('serial_number',$datas->Serial)->first();
         echo "<pre>";
 
         print_r($datas);
-        // print_r($stock);
+        print_r($stock);
         echo "</pre>";
         }
 
