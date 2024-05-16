ca8e7f930a397111ee5692b3e9bd1fd122e76c33
haleem24 <haleem.shah24@gmail.com>
2024-05-16 19:02:58 +0100

    AfterSale

--- app/Exports/PacksheetExport.php
+++ app/Exports/PacksheetExport.php
@@ -29,7 +29,7 @@ class PacksheetExport implements FromCollection, WithHeadings
         })
 
         ->select(
-            DB::raw('(@row_number:=@row_number + 1) as row_number'),
+            DB::raw('(@rn:=@rn + 1) AS row_number'),
             'products.model',
             'storage.name as storage',
             'color.name as color',
@@ -39,7 +39,7 @@ class PacksheetExport implements FromCollection, WithHeadings
             'stock_operations.description as issue',
             'order_items.price as price'
         )
-        ->crossJoin(DB::raw('(SELECT @row_number := 0) AS rn'))
+        ->crossJoin(DB::raw('(SELECT @rn := 0) AS init'))
         ->where('orders.id', request('id'))
         ->where('orders.deleted_at',null)
         ->where('order_items.deleted_at', null)
