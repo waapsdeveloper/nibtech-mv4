d5663f502eb3f1c6ebd3906b2de5e265891be558
haleem24 <haleem.shah24@gmail.com>
2024-05-21 11:53:43 +0100

    Exchange Rates API

--- app/Models/ExchangeRate.php
+++ app/Models/ExchangeRate.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ExchangeRate extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        // other fields...
+        'base_currency',
+    ];
 }
