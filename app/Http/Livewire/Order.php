066cb03cd29cf4fabf4cc6d8e5ff3be98f450391
haleem24 <haleem.shah24@gmail.com>
2024-05-22 11:45:36 +0100

    Exchange Rates API

--- app/Http/Livewire/Order.php
+++ app/Http/Livewire/Order.php
@@ -846,12 +846,15 @@ class Order extends Component
 
         // dd($pdfContent);
         // Send the invoice via email
+
         Mail::to($order->customer->email)->send(new InvoiceMail($data));
+        if(session('user_id') == 1){
 
-        $recipientEmail = 'wethesd@gmail.com';
-        $subject = 'Invoice for Your Recent Purchase';
+            $recipientEmail = 'wethesd@gmail.com';
+            $subject = 'Invoice for Your Recent Purchase';
 
-        $email = app(GoogleController::class)->sendEmailInvoice($recipientEmail, $subject, new InvoiceMail($data));
+            $email = app(GoogleController::class)->sendEmailInvoice($recipientEmail, $subject, new InvoiceMail($data));
+        }
         // Optionally, save the PDF locally
         // file_put_contents('invoice.pdf', $pdfContent);
 
