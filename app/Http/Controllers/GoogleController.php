d32ce31e06525f435bf8a2b9ef1d93d8b7601d89
haleem24 <haleem.shah24@gmail.com>
2024-05-22 13:01:46 +0100

    Exchange Rates API

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -158,6 +158,7 @@ class GoogleController extends Controller
         $rawMessageString .= $mailable->render() . "\r\n\r\n"; // Get the HTML content from the mailable
 
         // Attach the PDF
+        dd($mailable->build());
         $pdfData = $mailable->build()->attachments[0]['data'];
         $fileName = $mailable->build()->attachments[0]['as'];
         $fileType = $mailable->build()->attachments[0]['mime'];
