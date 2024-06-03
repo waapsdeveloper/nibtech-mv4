313f31902a2104392676aa766f3e748d01ca50ef
haleem24 <haleem.shah24@gmail.com>
2024-06-03 14:34:09 +0100

    Testing Page

--- app/Http/Livewire/Index.php
+++ app/Http/Livewire/Index.php
@@ -222,7 +222,7 @@ class Index extends Component
         $q->whereHas('order', function ($q) {
             $q->where('status', 3)
               ->where('order_type_id', 3)
-              ->whereColumn('order_items.reference_id', 'orders.reference_id');
+              ->whereColumn('orders.reference_id','order_items.reference_id');
         });
     })
     ->count();
