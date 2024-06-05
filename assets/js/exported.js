330791e3bbc483d276fed8c658758ff62d99417b
haleem24 <haleem.shah24@gmail.com>
2024-06-05 21:02:32 +0100

    Testing Page

--- assets/js/exported.js
+++ assets/js/exported.js
@@ -608,95 +608,95 @@
   } //Visitors chart
 
 
-  function statistics3 (data1,data2,dates) {
+  // function statistics3 (data1,data2,dates) {
 
-    var options1 = {
-      series: [{
-        name: 'EUR',
-        data: data1.split(',')
-      }, {
-        name: 'Orders',
-        data: data2.split(',')
-      }],
-      chart: {
-        type: 'line',
-        height: 280
-      },
-      grid: {
-        borderColor: '#f2f6f7'
-      },
-      colors: [myVarVal || "#38cab3"],
-      plotOptions: {
-        line: {
-          colors: {
-            ranges: [{
-              from: -100,
-              to: -46,
-              color: 'red'
-            }, {
-              from: -45,
-              to: 0,
-              color: 'green'
-            }]
-          },
-          columnWidth: '100%'
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        show: true,
-        width: 4,
-        colors: ['transparent']
-      },
-      legend: {
-        show: true,
-        position: 'top'
-      },
-      yaxis: {
-        title: {
-          text: 'Orders',
-          style: {
-            color: '#adb5be',
-            fontSize: '14px',
-            fontFamily: 'poppins, sans-serif',
-            fontWeight: 600,
-            cssClass: 'apexcharts-yaxis-label'
-          }
-        },
-        labels: {
-          formatter: function formatter(y) {
-            return y.toFixed(0) + "";
-          }
-        }
-      },
-      xaxis: {
-        type: 'month',
-        categories: dates.split(','),
-        axisBorder: {
-          show: true,
-          color: 'rgba(119, 119, 142, 0.05)',
-          offsetX: 0,
-          offsetY: 0
-        },
-        axisTicks: {
-          show: true,
-          borderType: 'solid',
-          color: 'rgba(119, 119, 142, 0.05)',
-          width: 6,
-          offsetX: 0,
-          offsetY: 0
-        },
-        labels: {
-          rotate: -90
-        }
-      }
-    };
-    document.getElementById('statistics2').innerHTML = '';
-    var chart1 = new ApexCharts(document.querySelector("#statistics2"), options1);
-    chart1.render();
-  } //Visitors chart
+  //   var options1 = {
+  //     series: [{
+  //       name: 'EUR',
+  //       data: data1.split(',')
+  //     }, {
+  //       name: 'Orders',
+  //       data: data2.split(',')
+  //     }],
+  //     chart: {
+  //       type: 'line',
+  //       height: 280
+  //     },
+  //     grid: {
+  //       borderColor: '#f2f6f7'
+  //     },
+  //     colors: [myVarVal || "#38cab3"],
+  //     plotOptions: {
+  //       line: {
+  //         colors: {
+  //           ranges: [{
+  //             from: -100,
+  //             to: -46,
+  //             color: 'red'
+  //           }, {
+  //             from: -45,
+  //             to: 0,
+  //             color: 'green'
+  //           }]
+  //         },
+  //         columnWidth: '100%'
+  //       }
+  //     },
+  //     dataLabels: {
+  //       enabled: false
+  //     },
+  //     stroke: {
+  //       show: true,
+  //       width: 4,
+  //       colors: ['transparent']
+  //     },
+  //     legend: {
+  //       show: true,
+  //       position: 'top'
+  //     },
+  //     yaxis: {
+  //       title: {
+  //         text: 'Orders',
+  //         style: {
+  //           color: '#adb5be',
+  //           fontSize: '14px',
+  //           fontFamily: 'poppins, sans-serif',
+  //           fontWeight: 600,
+  //           cssClass: 'apexcharts-yaxis-label'
+  //         }
+  //       },
+  //       labels: {
+  //         formatter: function formatter(y) {
+  //           return y.toFixed(0) + "";
+  //         }
+  //       }
+  //     },
+  //     xaxis: {
+  //       type: 'month',
+  //       categories: dates.split(','),
+  //       axisBorder: {
+  //         show: true,
+  //         color: 'rgba(119, 119, 142, 0.05)',
+  //         offsetX: 0,
+  //         offsetY: 0
+  //       },
+  //       axisTicks: {
+  //         show: true,
+  //         borderType: 'solid',
+  //         color: 'rgba(119, 119, 142, 0.05)',
+  //         width: 6,
+  //         offsetX: 0,
+  //         offsetY: 0
+  //       },
+  //       labels: {
+  //         rotate: -90
+  //       }
+  //     }
+  //   };
+  //   document.getElementById('statistics2').innerHTML = '';
+  //   var chart1 = new ApexCharts(document.querySelector("#statistics2"), options1);
+  //   chart1.render();
+  // } //Visitors chart
 
 
    function viewers() {
@@ -781,90 +781,90 @@
    //Sales Activity
 
 
-   function statistics2() {
-     setTimeout(function () {
-       var options = {
-         series: [{
-           name: "Sales",
-           data: [32, 15, 63, 51, 136, 62, 99, 42, 178, 76, 32, 180]
-         }],
-         chart: {
-           height: 280,
-           type: 'line',
-           zoom: {
-             enabled: false
-           },
-           dropShadow: {
-             enabled: true,
-             enabledOnSeries: undefined,
-             top: 5,
-             left: 0,
-             blur: 3,
-             color: '#000',
-             opacity: 0.1
-           }
-         },
-         dataLabels: {
-           enabled: false
-         },
-         legend: {
-           position: "top",
-           horizontalAlign: "left",
-           offsetX: -15,
-           fontWeight: "bold"
-         },
-         stroke: {
-           curve: 'smooth',
-           width: '3'
-         },
-         grid: {
-           borderColor: '#f2f6f7'
-         },
-         colors: [myVarVal || "#1fc5db"],
-         yaxis: {
-           title: {
-             text: 'Growth',
-             style: {
-               color: '#adb5be',
-               fontSize: '14px',
-               fontFamily: 'poppins, sans-serif',
-               fontWeight: 600,
-               cssClass: 'apexcharts-yaxis-label'
-             }
-           },
-           labels: {
-             formatter: function formatter(y) {
-               return y.toFixed(0) + "";
-             }
-           }
-         },
-         xaxis: {
-           type: 'number',
-           categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-           axisBorder: {
-             show: true,
-             color: 'rgba(119, 119, 142, 0.05)',
-             offsetX: 0,
-             offsetY: 0
-           },
-           axisTicks: {
-             show: true,
-             borderType: 'solid',
-             color: 'rgba(119, 119, 142, 0.05)',
-             width: 6,
-             offsetX: 0,
-             offsetY: 0
-           },
-           labels: {
-             rotate: -90
-           }
-         }
-       };
-       document.getElementById('statistics2').innerHTML = '';
-       var chart = new ApexCharts(document.querySelector("#statistics2"), options);
-       chart.render();
-     }, 300);
-   } // Budget Chart
+  //  function statistics2() {
+  //    setTimeout(function () {
+  //      var options = {
+  //        series: [{
+  //          name: "Sales",
+  //          data: [32, 15, 63, 51, 136, 62, 99, 42, 178, 76, 32, 180]
+  //        }],
+  //        chart: {
+  //          height: 280,
+  //          type: 'line',
+  //          zoom: {
+  //            enabled: false
+  //          },
+  //          dropShadow: {
+  //            enabled: true,
+  //            enabledOnSeries: undefined,
+  //            top: 5,
+  //            left: 0,
+  //            blur: 3,
+  //            color: '#000',
+  //            opacity: 0.1
+  //          }
+  //        },
+  //        dataLabels: {
+  //          enabled: false
+  //        },
+  //        legend: {
+  //          position: "top",
+  //          horizontalAlign: "left",
+  //          offsetX: -15,
+  //          fontWeight: "bold"
+  //        },
+  //        stroke: {
+  //          curve: 'smooth',
+  //          width: '3'
+  //        },
+  //        grid: {
+  //          borderColor: '#f2f6f7'
+  //        },
+  //        colors: [myVarVal || "#1fc5db"],
+  //        yaxis: {
+  //          title: {
+  //            text: 'Growth',
+  //            style: {
+  //              color: '#adb5be',
+  //              fontSize: '14px',
+  //              fontFamily: 'poppins, sans-serif',
+  //              fontWeight: 600,
+  //              cssClass: 'apexcharts-yaxis-label'
+  //            }
+  //          },
+  //          labels: {
+  //            formatter: function formatter(y) {
+  //              return y.toFixed(0) + "";
+  //            }
+  //          }
+  //        },
+  //        xaxis: {
+  //          type: 'number',
+  //          categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  //          axisBorder: {
+  //            show: true,
+  //            color: 'rgba(119, 119, 142, 0.05)',
+  //            offsetX: 0,
+  //            offsetY: 0
+  //          },
+  //          axisTicks: {
+  //            show: true,
+  //            borderType: 'solid',
+  //            color: 'rgba(119, 119, 142, 0.05)',
+  //            width: 6,
+  //            offsetX: 0,
+  //            offsetY: 0
+  //          },
+  //          labels: {
+  //            rotate: -90
+  //          }
+  //        }
+  //      };
+  //      document.getElementById('statistics2').innerHTML = '';
+  //      var chart = new ApexCharts(document.querySelector("#statistics2"), options);
+  //      chart.render();
+  //    }, 300);
+  //  } // Budget Chart
 
 
    function budget() {
