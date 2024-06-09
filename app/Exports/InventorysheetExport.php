da1d5a955545155935e018229793ac5bdd8fc9b6
haleem24 <haleem.shah24@gmail.com>
2024-06-09 15:13:33 +0100

    Testing Page

--- app/Exports/InventorysheetExport.php
+++ app/Exports/InventorysheetExport.php
@@ -30,7 +30,7 @@ class InventorysheetExport implements FromCollection, WithHeadings
         //          ->whereColumn('stock_operations.new_variation_id', 'stock.variation_id')
         //          ->orderBy('id','desc')->limit(1);
         // })
-        ->leftJoin(DB::raw('(SELECT stock_id, new_variation_id, MAX(id) as max_id FROM stock_operations GROUP BY stock_id, new_variation_id) as stock_operations'), function($join) {
+        ->leftJoin(DB::raw('(SELECT stock_id, new_variation_id, description, MAX(id) as max_id FROM stock_operations GROUP BY stock_id, new_variation_id) as stock_operations'), function($join) {
             $join->on('stock.id', '=', 'stock_operations.stock_id')
                  ->whereColumn('stock_operations.new_variation_id', 'stock.variation_id');
         })
