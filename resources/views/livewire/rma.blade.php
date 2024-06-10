bf915480e52438e147f3927c4b50dc6d4b612474
haleem24 <haleem.shah24@gmail.com>
2024-06-10 14:16:42 +0100

    Testing Page

--- resources/views/livewire/rma.blade.php
+++ resources/views/livewire/rma.blade.php
@@ -221,7 +221,7 @@
                         <input type="hidden" name="rma[type]" id="" value="5">
                         <div class="form-group">
                             <label for="">Reference ID</label>
-                            <input class="form-control" placeholder="input Reference No" name="rma[reference_id]" type="text" value="{{ $latest_reference }}" readonly required>
+                            <input class="form-control" placeholder="input Reference No" name="rma[reference_id]" type="text" value="{{ $latest_reference+1 }}" readonly required>
                         </div>
                         <div class="form-group">
                             <label for="">Vendor</label>
