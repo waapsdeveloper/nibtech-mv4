ed75b4ce1e54e2c2f76de7b36f2fd527afb0b599
haleem24 <haleem.shah24@gmail.com>
2024-07-01 15:42:57 +0100

    misc

--- app/Http/Livewire/Listing.php
+++ app/Http/Livewire/Listing.php
@@ -54,7 +54,7 @@ class Listing extends Component
             return $q->where('grade', request('grade'));
         })
         ->with(['product' => function ($q) {
-            $q->orderBy('brand')->orderBy('model')->orderBy('category');
+            $q->orderBy('category');
         }])
 
 
