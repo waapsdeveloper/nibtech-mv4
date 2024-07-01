1aff637445b7b7cfa18e33bfa3876112516b2540
haleem24 <haleem.shah24@gmail.com>
2024-07-01 10:26:37 +0100

    misc

--- resources/views/livewire/listing.blade.php
+++ resources/views/livewire/listing.blade.php
@@ -196,7 +196,7 @@
                                         $sign = $listing->currency->sign;
                                     @endphp
                                         <tr @if ($listing->buybox != 1) style="background: LightRed;" @endif>
-                                            <td><img src="{{ asset('assets/img/flags/').'/'.strtolower($listing->country_id->code).'.svg' }}" height="10"> {{ $listing->country_id->code }}</td>
+                                            <td><img src="{{ asset('assets/img/flags/').'/'.strtolower($listing->country_id->code).'.svg' }}" height="15"> {{ $listing->country_id->code }}</td>
                                             @if (session('user')->hasPermission('view_price'))
                                             <td>{{$sign.$listing->min_price}}</td>
                                             <td>{{$sign.$listing->max_price}}</td>
