e75a7865f6aad60ce9657a459f5699c144b4cc5c
haleem24 <haleem.shah24@gmail.com>
2024-07-02 11:00:17 +0100

    misc

--- resources/views/livewire/listing.blade.php
+++ resources/views/livewire/listing.blade.php
@@ -295,7 +295,7 @@
                                     @endphp
                                         <tr>
                                             <td>{{ $i }}</td>
-                                            <a href="{{ url('imei?imei=').$item->imei.$item->serial_number }}"><td data-stock="{{ $item->id }}">{{ $item->imei.$item->serial_number }}</td></a>
+                                            <td data-stock="{{ $item->id }}"><a href="{{ url('imei?imei=').$item->imei.$item->serial_number }}">{{ $item->imei.$item->serial_number }}</a></td>
                                             @if (session('user')->hasPermission('view_cost'))
                                             <td>€{{$item->purchase_item->price ?? "Error in Purchase Entry" }}</td>
                                             @endif
