10687fe87056d249889743fee291ef255c660c8c
haleem24 <haleem.shah24@gmail.com>
2024-07-08 11:11:23 +0100

    listing

--- app/Models/Variation_model.php
+++ app/Models/Variation_model.php
@@ -68,7 +68,9 @@ class Variation_model extends Model
     }
     public function pending_orders()
     {
-        return $this->hasMany(Order_item_model::class, 'variation_id', 'id')->where('status',2)->whereHas('sale_order');
+        return $this->hasMany(Order_item_model::class, 'variation_id', 'id')->where('status',2)->whereHas('order', function($q){
+            $q->where('order_type_id',3);
+        });
     }
     public function update_qty($bm)
     {
