ff654975f725bc0fc543942cc85d9b4c3734db37
haleem24 <haleem.shah24@gmail.com>
2024-07-09 11:10:33 +0100

    listing

--- app/Console/Commands/RefreshLatest.php
+++ app/Console/Commands/RefreshLatest.php
@@ -49,30 +49,30 @@ class RefreshLatest extends Command
         $currency_codes = Currency_model::pluck('id','code');
         $country_codes = Country_model::pluck('id','code');
 
-        $resArray1 = $bm->getNewOrders();
-        $orders = [];
-        if ($resArray1 !== null) {
-            foreach ($resArray1 as $orderObj) {
-                if (!empty($orderObj)) {
-                    foreach($orderObj->orderlines as $orderline){
-                        $this->validateOrderlines($orderObj->order_id, $orderline->listing, $bm);
-                    }
-                    $orders[] = $orderObj->order_id;
-                }
-            }
-            foreach($orders as $or){
-                $this->updateBMOrder($or, $bm, $currency_codes, $country_codes, $order_model, $order_item_model);
-            }
-        }
-        $orders = Order_model::whereIn('status', [0, 1])
-            ->orWhereNull('delivery_note_url')
-            ->orWhereNull('label_url')
-            ->where('order_type_id', 3)
-            ->where('created_at', '>=', Carbon::now()->subDays(2))
-            ->pluck('reference_id');
-        foreach($orders as $order){
-            $this->updateBMOrder($order, $bm, $currency_codes, $country_codes, $order_model, $order_item_model);
-        }
+        // $resArray1 = $bm->getNewOrders();
+        // $orders = [];
+        // if ($resArray1 !== null) {
+        //     foreach ($resArray1 as $orderObj) {
+        //         if (!empty($orderObj)) {
+        //             foreach($orderObj->orderlines as $orderline){
+        //                 $this->validateOrderlines($orderObj->order_id, $orderline->listing, $bm);
+        //             }
+        //             $orders[] = $orderObj->order_id;
+        //         }
+        //     }
+        //     foreach($orders as $or){
+        //         $this->updateBMOrder($or, $bm, $currency_codes, $country_codes, $order_model, $order_item_model);
+        //     }
+        // }
+        // $orders = Order_model::whereIn('status', [0, 1])
+        //     ->orWhereNull('delivery_note_url')
+        //     ->orWhereNull('label_url')
+        //     ->where('order_type_id', 3)
+        //     ->where('created_at', '>=', Carbon::now()->subDays(2))
+        //     ->pluck('reference_id');
+        // foreach($orders as $order){
+        //     $this->updateBMOrder($order, $bm, $currency_codes, $country_codes, $order_model, $order_item_model);
+        // }
 
 
         $last_id = Order_item_model::where('care_id','!=',null)->where('created_at','>=',Carbon::now()->subDays(4))->whereHas('sale_order')->orderBy('reference_id','asc')->first()->care_id;
