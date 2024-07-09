c9ad30b42c90d4a246af91709b151652db4bd282
haleem24 <haleem.shah24@gmail.com>
2024-07-09 10:53:18 +0100

    listing

--- app/Console/Commands/RefreshLatest.php
+++ app/Console/Commands/RefreshLatest.php
@@ -76,10 +76,10 @@ class RefreshLatest extends Command
 
 
         $last_id = Order_item_model::where('care_id','!=',null)->where('created_at','>=',Carbon::now()->subDays(4))->whereHas('sale_order')->orderBy('reference_id','asc')->first()->care_id;
-        // echo $last_id;
+        echo $last_id;
         $care = $bm->getAllCare(false, ['last_id'=>$last_id,'page-size'=>50]);
         // $care = $bm->getAllCare(false, ['page-size'=>50]);
-        // print_r($care);
+        print_r($care);
         $care_line = collect($care)->pluck('id','orderline')->toArray();
         $care_keys = array_keys($care_line);
 
