14788a6efaacf35656af80d85808f7c38bba561f
haleem24 <haleem.shah24@gmail.com>
2024-07-10 21:34:27 +0100

    listing

--- app/Console/Commands/Functions.php
+++ app/Console/Commands/Functions.php
@@ -88,14 +88,14 @@ class Functions extends Command
         //     }
         // }
 
-        $variations_2 = Variation_model::where('sku','!=',null)->where('product_id','!=',null)->limit(1000)->withTrashed()->get()->pluck('id');
+        $variations_2 = Variation_model::where('sku','!=',null)->where('product_id','!=',null)->withTrashed()->get()->pluck('id');
         echo $variations_2->count();
         die;
         if(file_exists('variations_2.txt')){
             $last_id = file_get_contents('variations_2.txt');
-            $variations_2 = Variation_model::where('id','>',$last_id)->where('sku','!=',null)->where('product_id','!=',null)->limit(1000)->withTrashed()->get()->pluck('id');
+            $variations_2 = Variation_model::where('id','>',$last_id)->where('sku','!=',null)->where('product_id','!=',null)->withTrashed()->get()->pluck('id');
             if($variations_2->count() == 0){
-                $variations_2 = Variation_model::where('sku','!=',null)->where('product_id','!=',null)->limit(1000)->withTrashed()->get()->pluck('id');
+                $variations_2 = Variation_model::where('sku','!=',null)->where('product_id','!=',null)->withTrashed()->get()->pluck('id');
             }
         }
         foreach($variations_2 as $id){
