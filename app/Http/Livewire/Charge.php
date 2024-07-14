d9f4bfbc8086523af33794dd4e1c433cb8ff161d
haleem24 <haleem.shah24@gmail.com>
2024-07-14 19:46:26 +0100

    listing

--- app/Http/Livewire/Charge.php
+++ app/Http/Livewire/Charge.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Http\Controllers\GoogleController;
 use App\Models\Charge_frequency_model;
 use App\Models\Charge_model;
+use App\Models\Charge_value_model;
 use App\Models\Country_model;
 use Livewire\Component;
 use App\Models\Multi_type_model;
@@ -38,18 +39,31 @@ class Charge extends Component
     }
     public function add_charge()
     {
+        $data = request('charge');
+        $charge = Charge_model::firstOrNew(['charge_frequency_id'=>$data['charge_frequency'],'order_type_id'=>$data['order_type'],'payment_method_id'=>$data['payment_method'],'name'=>$data['name'],'amount_type'=>$data['amount_type']]);
+        $charge->description = $data['description'];
+        $charge->status = 1;
+        $charge->save();
 
-        $data['countries'] = Country_model::all();
-        return view('livewire.add-charge')->with($data);
-    }
+        $charge_value = Charge_value_model::firstOrNew(['charge_id'=>$charge->id, 'ended_at'=>null]);
+        if($charge_value->id != null){
+            $charge_value->ended_at = Date('Y-m-d H:i:s', strtotime('-1 second', strtotime($data['start_date'])));
+            $charge_value->save();
 
-    public function insert_charge()
-    {
+            $charge_value = Charge_value_model::firstOrNew(['charge_id'=>$charge->id, 'ended_at'=>null]);
+            if($charge_value->id != null){
+                $charge_value->ended_at = Date('Y-m-d H:i:s', strtotime('-1 second', strtotime($data['start_date'])));
+                $charge_value->save();
 
+                $charge_value = Charge_value_model::firstOrNew(['charge_id'=>$charge->id, 'ended_at'=>null]);
+            }
+        }
+        $charge_value->value = $data['value'];
+        $charge_value->started_at = $data['start_date'];
+        $charge_value->save();
 
-        Charge_model::insert(['name'=>request('name')]);
         session()->put('success',"Charge has been added successfully");
-        return redirect('charge');
+        return redirect()->back();
     }
 
     public function edit_charge($id)
