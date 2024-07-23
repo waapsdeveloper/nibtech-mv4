84affe5db9d67a034c35704fbfce907cff580b5c
haleem24 <haleem.shah24@gmail.com>
2024-07-23 14:04:38 +0100

    listing

--- resources/views/auth/2fa.blade.php
+++ resources/views/auth/2fa.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -77,4 +77,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
