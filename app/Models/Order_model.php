e600076e837777eb841ce037f863ee04e908656d
haleem24 <haleem.shah24@gmail.com>
2024-07-24 13:59:27 +0100

    listing

--- app/Models/Order_model.php
+++ app/Models/Order_model.php
@@ -100,7 +100,7 @@ class Order_model extends Model
             $order->order_type_id = 3;
             $order->price = $orderObj->price;
             $order->delivery_note_url = $orderObj->delivery_note;
-            if($order->label_url == null && $bm->getOrderLabel($orderObj->order_id) != null){
+            if($order->label_url == null && $bm->getOrderLabel($orderObj->order_id) != null && $bm->getOrderLabel($orderObj->order_id)->results != null){
                 if($bm->getOrderLabel($orderObj->order_id) != null){
                     $order->label_url = $bm->getOrderLabel($orderObj->order_id)->results[0]->labelUrl;
                 }
