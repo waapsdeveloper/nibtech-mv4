584cfd7303ec73bf844634a155a95ae146ff8579
haleem24 <haleem.shah24@gmail.com>
2024-07-30 20:45:21 +0100

    listing

--- app/Console/Commands/FunctionsDaily.php
+++ app/Console/Commands/FunctionsDaily.php
@@ -69,106 +69,106 @@ class FunctionsDaily extends Command
         $stocks = Stock_model::where('status','!=',null)->where('order_id','!=',null)->orderByDesc('id')->get();
         foreach($stocks as $stock){
 
-            $last_item = $stock->last_item();
+            // $last_item = $stock->last_item();
 
-            $items2 = Order_item_model::where(['stock_id'=>$stock->id,'linked_id'=>null])->whereHas('order', function ($query) {
-                $query->where('order_type_id', 1)->where('reference_id','<=',10009);
-            })->orderBy('id','asc')->get();
-            if($items2->count() > 1){
-                $i = 0;
-                foreach($items2 as $item2){
-                    $i ++;
-                    if($i == 1){
-                        $stock->order_id = $item2->order_id;
-                        $stock->save();
-                    }else{
-                        $item2->delete();
-                    }
-                }
-                $last_item = $stock->last_item();
-            }
-            if($stock->purchase_item){
+            // $items2 = Order_item_model::where(['stock_id'=>$stock->id,'linked_id'=>null])->whereHas('order', function ($query) {
+            //     $query->where('order_type_id', 1)->where('reference_id','<=',10009);
+            // })->orderBy('id','asc')->get();
+            // if($items2->count() > 1){
+            //     $i = 0;
+            //     foreach($items2 as $item2){
+            //         $i ++;
+            //         if($i == 1){
+            //             $stock->order_id = $item2->order_id;
+            //             $stock->save();
+            //         }else{
+            //             $item2->delete();
+            //         }
+            //     }
+            //     $last_item = $stock->last_item();
+            // }
+            // if($stock->purchase_item){
 
-                $items3 = Order_item_model::where(['stock_id'=>$stock->id, 'linked_id' => $stock->purchase_item->id])->whereHas('order', function ($query) {
-                    $query->whereIn('order_type_id', [5,3]);
-                })->orderBy('id','asc')->get();
-                if($items3->count() > 1){
-                    $i = 0;
-                    foreach($items3 as $item3){
-                        $i ++;
-                        if($i == 1){
-                        }else{
-                            $item3->linked_id = null;
-                            $item3->save();
-                        }
-                    }
-                }
+            //     $items3 = Order_item_model::where(['stock_id'=>$stock->id, 'linked_id' => $stock->purchase_item->id])->whereHas('order', function ($query) {
+            //         $query->whereIn('order_type_id', [5,3]);
+            //     })->orderBy('id','asc')->get();
+            //     if($items3->count() > 1){
+            //         $i = 0;
+            //         foreach($items3 as $item3){
+            //             $i ++;
+            //             if($i == 1){
+            //             }else{
+            //                 $item3->linked_id = null;
+            //                 $item3->save();
+            //             }
+            //         }
+            //     }
 
-            }
-            $items4 = Order_item_model::where(['stock_id'=>$stock->id])->whereHas('order', function ($query) {
-                $query->whereIn('order_type_id', [5,3]);
-            })->orderBy('id','asc')->get();
-            if($items4->count() == 1){
-                foreach($items4 as $item4){
-                    if($stock->purchase_item){
-                        if($item4->linked_id != $stock->purchase_item->id && $item4->linked_id != null){
-                            $item4->linked_id = $stock->purchase_item->id;
-                            $item4->save();
-                        }
-                    }
-                }
-            }
-            $items5 = Order_item_model::where(['stock_id'=>$stock->id,'linked_id'=>null])->whereHas('order', function ($query) {
-                $query->whereIn('order_type_id', [2,3,4,5]);
-            })->orderBy('id','asc')->get();
-            if($items5->count() == 1){
-                foreach($items5 as $item5){
-                    if($stock->last_item()){
+            // }
+            // $items4 = Order_item_model::where(['stock_id'=>$stock->id])->whereHas('order', function ($query) {
+            //     $query->whereIn('order_type_id', [5,3]);
+            // })->orderBy('id','asc')->get();
+            // if($items4->count() == 1){
+            //     foreach($items4 as $item4){
+            //         if($stock->purchase_item){
+            //             if($item4->linked_id != $stock->purchase_item->id && $item4->linked_id != null){
+            //                 $item4->linked_id = $stock->purchase_item->id;
+            //                 $item4->save();
+            //             }
+            //         }
+            //     }
+            // }
+            // $items5 = Order_item_model::where(['stock_id'=>$stock->id,'linked_id'=>null])->whereHas('order', function ($query) {
+            //     $query->whereIn('order_type_id', [2,3,4,5]);
+            // })->orderBy('id','asc')->get();
+            // if($items5->count() == 1){
+            //     foreach($items5 as $item5){
+            //         if($stock->last_item()){
 
-                        $last_item = $stock->last_item();
-                        $item5->linked_id = $last_item->id;
-                        $item5->save();
-                    }
-                }
-                    $last_item = $stock->last_item();
-            }
-                // if(session('user_id') == 1){
-                //     dd($last_item);
-                // }
+            //             $last_item = $stock->last_item();
+            //             $item5->linked_id = $last_item->id;
+            //             $item5->save();
+            //         }
+            //     }
+            //         $last_item = $stock->last_item();
+            // }
+            //     // if(session('user_id') == 1){
+            //     //     dd($last_item);
+            //     // }
 
-                $stock_id = $stock->id;
+            //     $stock_id = $stock->id;
 
-            $process_stocks = Process_stock_model::where('stock_id', $stock_id)->whereHas('process', function ($query) {
-                $query->where('process_type_id', 9);
-            })->orderBy('id','desc')->get();
-            $data['process_stocks'] = $process_stocks;
+            // $process_stocks = Process_stock_model::where('stock_id', $stock_id)->whereHas('process', function ($query) {
+            //     $query->where('process_type_id', 9);
+            // })->orderBy('id','desc')->get();
+            // $data['process_stocks'] = $process_stocks;
 
-            if($last_item){
+            // if($last_item){
 
-                if(in_array($last_item->order->order_type_id,[1,4])){
-                    $message = 'IMEI is Available';
-                    // if($stock->status == 2){
-                        if($process_stocks->where('status',1)->count() == 0){
-                            $stock->status = 1;
-                            $stock->save();
-                        }else{
-                            $stock->status = 2;
-                            $stock->save();
+            //     if(in_array($last_item->order->order_type_id,[1,4])){
+            //         $message = 'IMEI is Available';
+            //         // if($stock->status == 2){
+            //             if($process_stocks->where('status',1)->count() == 0){
+            //                 $stock->status = 1;
+            //                 $stock->save();
+            //             }else{
+            //                 $stock->status = 2;
+            //                 $stock->save();
 
-                            $message = "IMEI sent for repair";
-                        }
-                    // }else{
+            //                 $message = "IMEI sent for repair";
+            //             }
+            //         // }else{
 
-                    // }
-                }else{
-                    $message = "IMEI Sold";
-                    if($stock->status == 1){
-                        $stock->status = 2;
-                        $stock->save();
-                    }
-                }
-                    session()->put('success', $message);
-            }
+            //         // }
+            //     }else{
+            //         $message = "IMEI Sold";
+            //         if($stock->status == 1){
+            //             $stock->status = 2;
+            //             $stock->save();
+            //         }
+            //     }
+            //         session()->put('success', $message);
+            // }
         }
 
 
