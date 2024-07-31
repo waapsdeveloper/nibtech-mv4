30c79db92ecfbc4725fd13e896b1fcecaa57bec1
haleem24 <haleem.shah24@gmail.com>
2024-07-31 14:08:50 +0100

    listing

--- resources/views/livewire/wholesale_detail.blade.php
+++ resources/views/livewire/wholesale_detail.blade.php
@@ -84,7 +84,7 @@
         <!-- /breadcrumb -->
             <div class="text-center" style="border-bottom: 1px solid rgb(216, 212, 212);">
                     {{-- <center><h4>BulkSale Order Detail</h4></center> --}}
-                    <h5>Reference: {{ $order->reference_id }} | Purchaser: {{ $order->customer->first_name }} | Total Items: {{ $order->order_items->count() }} | Total Price: {{ $order->currency_id->sign.number_format($order->order_items->sum('price'),2) }} | Creation Date: {{ $order->created_at }} | Approval Date: {{ $order->processed_at }}</h5>
+                    <h5>Reference: {{ $order->reference_id }} | Purchaser: {{ $order->customer->first_name }} | Total Items: {{ $order->order_items->count() }} | Total Price: {{ $order->currency_id->sign.number_format($order->order_items->sum('price'),2) }} | Creation Date: {{ $order->created_at }} @if ($order->status == 3) | Approval Date: {{ $order->processed_at }} @endif</h5>
 
             </div>
         <br>
