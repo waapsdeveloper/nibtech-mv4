5903869afce011d5b1b0247873866d3d04fc875c
haleem24 <haleem.shah24@gmail.com>
2024-08-01 20:17:16 +0100

    listing

--- resources/views/livewire/pnl_new.blade.php
+++ resources/views/livewire/pnl_new.blade.php
@@ -24,10 +24,10 @@
                                 <th style="width: 200px;"><small><b>GBP Price</b></small></th>
                                 @endif
                                 @if (session('user')->hasPermission('view_cost'))
-                                    <th style="width: 250px;"><small><b>Cost</b></small></th>
+                                    <th style="width: 230px;"><small><b>Cost</b></small></th>
                                     <th style="width: 150px;"><small><b>Repair</b></small></th>
                                     <th style="width: 150px;"><small><b>Fee</b></small></th>
-                                    <th style="width: 250px;"><small><b>Profit</b></small></th>
+                                    <th style="width: 220px;"><small><b>Profit</b></small></th>
                                 @endif
                             </tr>
                         </thead>
