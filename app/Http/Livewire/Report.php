f70c08eab24bfa8a65487b82be184353d8237cc8
haleem24 <haleem.shah24@gmail.com>
2024-08-01 20:44:06 +0100

    listing

--- app/Http/Livewire/Report.php
+++ app/Http/Livewire/Report.php
@@ -62,8 +62,8 @@ class Report extends Component
         // $start_date = date('Y-m-d 00:00:00',);
         $end_date = date('Y-m-d 23:59:59');
         if (request('start_date') != NULL && request('end_date') != NULL) {
-            $start_date = request('start_date') . " 23:00:00";
-            $end_date = request('end_date') . " 22:59:59";
+            $start_date = request('start_date') . " 00:00:00";
+            $end_date = request('end_date') . " 23:59:59";
         }
 
 
