22796ba859fab7acdec95699850e13f2fb0107d9
haleem24 <haleem.shah24@gmail.com>
2024-08-02 15:16:52 +0100

    listing

--- app/Http/Livewire/Report.php
+++ app/Http/Livewire/Report.php
@@ -304,6 +304,7 @@ class Report extends Component
             ->Where('orders.deleted_at',null)
             ->Where('order_items.deleted_at',null)
             ->Where('stock.deleted_at',null)
+            ->Where('variation.deleted_at',null)
             ->Where('process_stock.deleted_at',null)
             ->whereIn('orders.status', [3,6])
             ->whereIn('order_items.status', [3,6])
