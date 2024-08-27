cbeb79e65ab42054db90df9e79c760035acd3993
haleem24 <haleem.shah24@gmail.com>
2024-08-27 11:42:09 +0100

    Invoice

--- app/Http/Livewire/Order.php
+++ app/Http/Livewire/Order.php
@@ -388,13 +388,13 @@ class Order extends Component
             return $q->where('orders.status', request('status'));
         })
         ->when(request('status') == 3 && request('stock') == 0, function ($query) {
-            return $query->having('available_stock', '=', 0);
+            return $query->having('order_items_available', '=', 0);
         })
         ->when(request('status') == 3 && request('stock') == 1, function ($query) {
-            return $query->having('available_stock', '>', 0);
+            return $query->having('order_items_available', '>', 0);
         })
         // ->when(!request('stock'), function ($query) {
-        //     return $query->having('available_stock');
+        //     return $query->having('order_items_available');
         // })
         // ->when(request('stock'), function ($q) {
         //     if (request('stock') == 0) {
