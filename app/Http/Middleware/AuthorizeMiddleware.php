ab9a09a00e7dd3bb51c9ebf3c14657b27ff86d97
haleem24 <haleem.shah24@gmail.com>
2024-09-12 12:49:48 +0100

    IP Restriction

--- app/Http/Middleware/AuthorizeMiddleware.php
+++ app/Http/Middleware/AuthorizeMiddleware.php
@@ -44,7 +44,7 @@ class AuthorizeMiddleware
         session()->put('user',$user);
         // Check if the user has the required permission for the current page
         if (!$user->hasPermission($currentRoute)) {
-            abort(403, "Hello \n\r  world".nl2br(). Inspiring::quote());
+            abort(403, "Hello \n\r  world".nl2br('dsa'). Inspiring::quote());
         }
 
         // Remove the 'page' session variable
