1e6c0220ba42e8303afcd6f492a1f2097f51be49
haleem24 <haleem.shah24@gmail.com>
2024-09-16 20:06:26 +0100

    IP Restriction

--- resources/views/livewire/report_password.blade.php
+++ resources/views/livewire/report_password.blade.php
@@ -15,6 +15,28 @@
 						</div>
 					</div>
 					<!-- /breadcrumb -->
+        @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <span class="alert-inner--icon"><i class="fe fe-thumbs-up"></i></span>
+            <span class="alert-inner--text"><strong>{{session('success')}}</strong></span>
+            <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <br>
+        @php
+        session()->forget('success');
+        @endphp
+        @endif
+        @if (session('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <span class="alert-inner--icon"><i class="fe fe-thumbs-down"></i></span>
+                <span class="alert-inner--text"><strong>{{session('error')}}</strong></span>
+                <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"><span aria-hidden="true">&times;</span></button>
+            </div>
+        <br>
+        @php
+        session()->forget('error');
+        @endphp
+        @endif
                     <hr style="border-bottom: 1px solid #000">
 					<!-- row -->
                     <div class="row">
