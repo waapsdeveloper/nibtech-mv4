5c522e8bee1e42cf3f54eaed7b253be751bedfc8
haleem24 <haleem.shah24@gmail.com>
2024-09-17 18:52:03 +0100

    IP Restriction

--- app/Http/Livewire/Order.php
+++ app/Http/Livewire/Order.php
@@ -125,7 +125,7 @@ class Order extends Component
             });
         })
         ->when(request('missing_refund'), function ($q) {
-            return $q->whereHas('order_items.linked' != null)->where('status', 6);
+            return $q->whereHas('order_items.linked')->where('status', 6);
         })
         ->when(request('order_id') != '', function ($q) {
             if(str_contains(request('order_id'),'<')){
