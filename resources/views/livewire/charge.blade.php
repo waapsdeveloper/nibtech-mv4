f9c247878079c6b2429ed6fbb64061ee6723aa10
haleem24 <haleem.shah24@gmail.com>
2024-09-19 19:41:10 +0100

    IP Restriction

--- resources/views/livewire/charge.blade.php
+++ resources/views/livewire/charge.blade.php
@@ -98,7 +98,7 @@
                                             <td>{{$charge->charge_frequency->name}}</td>
                                             <td>{{$charge->order_type->name}}</td>
                                             <td>{{$charge->payment_method->name ?? "All"}}</td>
-                                            <td>{{$charge->current_value->name}}</td>
+                                            <td>{{number_format($charge->current_value->value)}}</td>
                                             <td>
                                                 @if ($charge->status == 1)
                                                     <span class="badge badge-success">Active</span>
