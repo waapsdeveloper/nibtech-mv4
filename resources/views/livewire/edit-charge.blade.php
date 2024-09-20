c35561d40b0b4afa53a7073681e7c8379d6e79ed
haleem24 <haleem.shah24@gmail.com>
2024-09-20 20:59:59 +0100

    IP Restriction

--- resources/views/livewire/edit-charge.blade.php
+++ resources/views/livewire/edit-charge.blade.php
@@ -126,11 +126,11 @@
                     <input type="hidden" name="order_charge[charge_id]" value="{{$charge->id}}">
                     <h5>Change Charge Standard: </h5>
                     <div class="input-group">
-                        <label class="form-label align-items-center">Start Date: </label>
+                        <label class="form-label">Start Date: </label>
                         <input type="datetime-local" class="form-control" name="order_charge[start_date]" required>
                     </div>
                     <div class="input-group">
-                        <label class="form-label align-items-center">Value: </label>
+                        <label class="form-label">Value: </label>
                         <input type="number" step="0.01" class="form-control" name="order_charge[amount]" required>
                     </div>
                     <button class="btn btn-primary" >Add</button>
