c55a8fed4b3a6c9534c15b5de847353614e7cb4c
haleem24 <haleem.shah24@gmail.com>
2024-09-28 12:24:24 +0100

    ECommerce Report

--- app/Http/Livewire/Inventory.php
+++ app/Http/Livewire/Inventory.php
@@ -823,8 +823,8 @@ class Inventory extends Component
                         'description' => 'Variation changed during inventory verification',
                         'admin_id' => session('user_id'),
                     ]);
-                    session()->put('copy', 1);
                 }
+                    session()->put('copy', 1);
             }else{
                 session()->put('copy', 0);
                 session()->put('product_id', $stock->variation->product_id);
