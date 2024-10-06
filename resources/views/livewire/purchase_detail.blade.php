b29793403c5a126b81c1f4a494e02d35f04c070d
haleem24 <haleem.shah24@gmail.com>
2024-10-06 13:17:39 +0100

    ECommerce Report

--- resources/views/livewire/purchase_detail.blade.php
+++ resources/views/livewire/purchase_detail.blade.php
@@ -453,6 +453,39 @@
                                                         <button class="btn btn-primary m-0" name="add_imei" value="1">Insert</button>
 
                                                     </form>
+                                                    @elseif ($row->message == "Item already added in this order" || str_contain($row->message, "Item already available"))
+                                                    <form id="order_issues_{{$row->id}}" method="POST" action="{{ url('purchase/remove_issues') }}" class="form-inline">
+                                                        @csrf
+                                                        <input type="hidden" name="id" value="{{$row->id}}">
+                                                        <div class="form-floating">
+                                                            <input type="text" class="form-control" id="imei" name="imei" placeholder="Enter IMEI" required>
+                                                            <label for="imei">IMEI</label>
+                                                        </div>
+                                                        <div class="form-floating">
+                                                            <input type="text" list="variations" id="variation" name="variation" class="form-control" value="{{ $grouped_issue->name }}" required>
+                                                            <datalist id="variations">
+                                                                <option value="">Select</option>
+                                                                @foreach ($all_variations as $variation)
+                                                                    @php
+                                                                        if($variation->product_id){
+                                                                            $product = $products[$variation->product_id];
+                                                                        }else{
+                                                                            $product = null;
+                                                                        }
+                                                                        if($variation->storage){
+                                                                            $storage = $storages[$variation->storage];
+                                                                        }else{
+                                                                            $storage = null;
+                                                                        }
+                                                                    @endphp
+                                                                    <option value="{{$variation->id}}" @if(isset($_GET['variation']) && $variation->id == $_GET['variation']) {{'selected'}}@endif>{{ $product." ".$storage}}</option>
+                                                                @endforeach
+                                                            </datalist>
+                                                            <label for="variation">Variation</label>
+                                                        </div>
+                                                        <button class="btn btn-primary m-0" name="add_imei" value="1">Insert</button>
+
+                                                    </form>
                                                     @elseif ($row->message == "Additional Item")
                                                     <form id="order_issues_{{$row->id}}" method="POST" action="{{ url('purchase/remove_issues') }}" class="form-inline">
                                                         @csrf
