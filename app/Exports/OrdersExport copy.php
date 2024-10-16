a61d3d16d3882d02945d4f602ec23fe545683cae
haleem24 <haleem.shah24@gmail.com>
2024-10-16 09:00:13 +0100

    POS

--- app/Exports/OrdersExport copy.php
+++ app/Exports/OrdersExport copy.php
@@ -84,6 +84,7 @@ class OrdersExport
             })
             ->when(request('with_stock') == 2, function ($q) {
                 return $q->where('order_items.stock_id', 0);
+                dd('Hello');
             })
             // ->groupBy('variation.sku', 'variation.name', 'grade.name')
             ->orderBy('orders.reference_id', 'DESC')
