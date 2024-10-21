ffdd77884edb94d5177f376c24663ae2c54828cd
haleem24 <haleem.shah24@gmail.com>
2024-10-21 18:30:49 +0100

    POS

--- app/Exports/StockSummeryExport.php
+++ app/Exports/StockSummeryExport.php
@@ -73,103 +73,16 @@ class StockSummeryExport
             }
             $result[$product->category][$product->brand][] = $data;
         }
-        // Fetch data from the database
-        // $data = DB::table('orders')
-        //     ->join('order_items', 'orders.id', '=', 'order_items.order_id')
-        //     ->join('variation', 'order_items.variation_id', '=', 'variation.id')
-        //     ->join('products', 'variation.product_id', '=', 'products.id')
-        //     ->join('color', 'variation.color', '=', 'color.id')
-        //     ->join('storage', 'variation.storage', '=', 'storage.id')
-        //     ->join('grade', 'variation.grade', '=', 'grade.id')
-        //     ->select(
-        //         'orders.reference_id',
-        //         'variation.sku',
-        //         'order_items.quantity',
-        //         'products.model',
-        //         'color.name as color',
-        //         'storage.name as storage',
-        //         'grade.name as grade_name',
-        //         // DB::raw('SUM(order_items.quantity) as total_quantity')
-        //     )
-        //     ->where('orders.deleted_at',null)
-        //     ->where('orders.order_type_id',3)
-        //     ->when(request('start_date') != '', function ($q) {
-        //         return $q->where('orders.created_at', '>=', request('start_date', 0));
-        //     })
-        //     ->when(request('end_date') != '', function ($q) {
-        //         return $q->where('orders.created_at', '<=', request('end_date', 0) . " 23:59:59");
-        //     })
-        //     ->when(request('status') != '', function ($q) {
-        //         return $q->where('orders.status', request('status'));
-        //     })
-        //     ->when(request('order_id') != '', function ($q) {
-        //         if(str_contains(request('order_id'),'<')){
-        //             $order_ref = str_replace('<','',request('order_id'));
-        //             return $q->where('orders.reference_id', '<', $order_ref);
-        //         }elseif(str_contains(request('order_id'),'>')){
-        //             $order_ref = str_replace('>','',request('order_id'));
-        //             return $q->where('orders.reference_id', '>', $order_ref);
-        //         }elseif(str_contains(request('order_id'),'<=')){
-        //             $order_ref = str_replace('<=','',request('order_id'));
-        //             return $q->where('orders.reference_id', '<=', $order_ref);
-        //         }elseif(str_contains(request('order_id'),'>=')){
-        //             $order_ref = str_replace('>=','',request('order_id'));
-        //             return $q->where('orders.reference_id', '>=', $order_ref);
-        //         }elseif(str_contains(request('order_id'),'-')){
-        //             $order_ref = explode('-',request('order_id'));
-        //             return $q->whereBetween('orders.reference_id', $order_ref);
-        //         }elseif(str_contains(request('order_id'),',')){
-        //             $order_ref = explode(',',request('order_id'));
-        //             return $q->whereIn('orders.reference_id', $order_ref);
-        //         }elseif(str_contains(request('order_id'),' ')){
-        //             $order_ref = explode(' ',request('order_id'));
-        //             return $q->whereIn('orders.reference_id', $order_ref);
-        //         }else{
-        //             return $q->where('orders.reference_id', 'LIKE', request('order_id') . '%');
-        //         }
-        //     })
-        //     ->when(request('last_order') != '', function ($q) {
-        //         return $q->where('orders.reference_id', '>', request('last_order'));
-        //     })
-        //     ->when(request('sku') != '', function ($q) {
-        //         return $q->whereHas('order_items.variation', function ($q) {
-        //             $q->where('sku', 'LIKE', '%' . request('sku') . '%');
-        //         });
-        //         // where('orders.order_items.variation.sku', 'LIKE', '%' . request('sku') . '%');
-        //     })
-        //     ->when(request('imei') != '', function ($q) {
-        //         return $q->whereHas('order_items.stock', function ($q) {
-        //             $q->where('imei', 'LIKE', '%' . request('imei') . '%');
-        //         });
-        //     })
-        //     ->when(request('missing') == 'scan', function ($q) {
-        //         return $q->whereIn('orders.status', [3,6])->whereNull('orders.scanned')->where('orders.processed_at', '<=', now()->subHours(48));
-        //     })
-        //     ->when(request('with_stock') == 2, function ($q) {
-        //         return $q->where('order_items.stock_id', 0);
-        //     })
-        //     // ->groupBy('variation.sku', 'variation.name', 'grade.name')
-        //     ->orderBy('orders.reference_id', 'DESC')
-        //     ->distinct()->get();
-
         // Create a TCPDF instance
         $pdf = new TCPDF();
         $pdf->SetMargins(10, 10, 10);
-
-        // $pdf->setPrintHeader(false);
-        // $pdf->SetFooterMargin(0);
-        // $pdf->setPrintFooter(false);
-
-        // Create a BarcodeGenerator instance
-        // $barcodeGenerator = new BarcodeGeneratorPNG();
-        // Iterate through data and add to PDF
 
         foreach($result as $category => $cat){
             // Add heading cell at the top center
             foreach($cat as $brand => $datas){
                 // Add a new page
                 $pdf->AddPage();
-                $pdf->SetFont('times', 'B', 12);
+                $pdf->SetFont('times', 'B', 16);
                 $pdf->Cell(0, 15, $categories[$category] . " - " . $brands[$brand], 0, 1, 'C');
                 $pdf->SetAutoPageBreak(TRUE, 15);
                 // Set font
@@ -199,13 +112,13 @@ class StockSummeryExport
                     $pdf->MultiCell(8, 0, $i, 1, 'L', false, 0, '', '', true);
                     $variationName = $this->ellipsize($data['model'], 40);
                     // $pdf->Cell(80, 0, $variationName, 1);
-                    $pdf->MultiCell(80, 0, $variationName, 1, 'L', false, 0, '', '', true);
-                    $pdf->MultiCell(12, 0, $data['stock_count'], 1, 'C', false, 0, '', '', true);
-                    $pdf->MultiCell(18, 0, number_format($data['average_cost'],2), 1, 'C', false, 0, '', '', true);
-                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][1] ?? 0), 1, 'C', false, 0, '', '', true);
-                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][2] ?? 0), 1, 'C', false, 0, '', '', true);
-                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][3] ?? 0), 1, 'C', false, 0, '', '', true);
-                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][5] ?? 0), 1, 'C', false, 0, '', '', true);
+                    $pdf->MultiCell(80, 0, $variationName, 1, 'L', false, 1, '', '', true);
+                    $pdf->MultiCell(12, 0, $data['stock_count'], 1, 'C', false, 1, '', '', true);
+                    $pdf->MultiCell(18, 0, number_format($data['average_cost'],2), 1, 'C', false, 1, '', '', true);
+                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][1] ?? 0), 1, 'C', false, 1, '', '', true);
+                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][2] ?? 0), 1, 'C', false, 1, '', '', true);
+                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][3] ?? 0), 1, 'C', false, 1, '', '', true);
+                    $pdf->MultiCell(18, 0, $this->bold($data['graded_average_cost'][5] ?? 0), 1, 'C', false, 1, '', '', true);
 
                 }
             }
