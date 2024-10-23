aee0d3cfef5608824135a3c7b9e72216894733a9
haleem24 <haleem.shah24@gmail.com>
2024-10-23 13:27:58 +0100

    POS

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -11,6 +11,6 @@
             </div>
         </div>
     </div>
-    @livewire('<reports>sales-report')
-    @livewire('batch-grade-report')
+    @livewire('reports.sales-report')
+    @livewire('reports.batch-grade-report')
 @endsection
