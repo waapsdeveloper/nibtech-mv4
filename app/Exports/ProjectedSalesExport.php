bddf6aabb7d612e4433793add14b127bea76cc88
haleem24 <haleem.shah24@gmail.com>
2024-10-26 15:05:09 +0100

    POS

--- app/Exports/ProjectedSalesExport.php
+++ app/Exports/ProjectedSalesExport.php
@@ -70,7 +70,7 @@ class ProjectedSalesExport
                 // Add a new page
                 $pdf->AddPage('L');
                 $pdf->SetFont('times', 'B', 16);
-                $pdf->Cell(0, 15, $categories[$category] . " - " . $brands[$brand], 0, 1, 'C');
+                $pdf->Cell(0, 12, $categories[$category] . " - " . $brands[$brand], 0, 1, 'C');
                 $pdf->SetAutoPageBreak(TRUE, 15);
                 // Set font
 
