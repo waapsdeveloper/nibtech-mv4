88994c5deaa5573768b3fdbe8f67fcaeb98daa84
haleem24 <haleem.shah24@gmail.com>
2024-10-30 23:49:31 +0000

    Sub Grade

--- resources/views/livewire/listing.blade.php
+++ resources/views/livewire/listing.blade.php
@@ -273,7 +273,10 @@
 
                         </span>
                         <script>
-                            $('#sales_{{$variation->id}}').load("{{url('listing/get_sales').'/'.$variation->id}}");
+                            $(document).load(function () {
+                                $('#sales_{{$variation->id}}').load("{{url('listing/get_sales').'/'.$variation->id}}");
+                            });
+                            // $('#sales_{{$variation->id}}').load("{{url('listing/get_sales').'/'.$variation->id}}");
                         </script>
                         <div>
                             <form class="form-inline" method="POST" id="change_qty_{{$variation->id}}" action="{{url('listing/update_quantity').'/'.$variation->id}}">
