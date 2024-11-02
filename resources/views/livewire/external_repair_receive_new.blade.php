eb39e52546aa85a00cb30b6c13f8279bfce2cded
haleem24 <haleem.shah24@gmail.com>
2024-11-02 13:26:04 +0000

    Sub Grade

--- resources/views/livewire/external_repair_receive_new.blade.php
+++ resources/views/livewire/external_repair_receive_new.blade.php
@@ -78,9 +78,6 @@ session()->forget('error');
         <div class="card-header pb-0">
             <div class="d-flex justify-content-between">
                 <h4 class="card-title mg-b-0">Latest Scanned</h4>
-                <h4 class="card-title mg-b-0">Counter: {{ session('counter') }} <a href="{{ url('stock_room/reset_counter') }}">Reset</a></h4>
-
-                <h4 class="card-title mg-b-0">Total Scanned: {{$scanned_total}}</h4>
             </div>
         </div>
         <div class="card-body"><div class="table-responsive">
