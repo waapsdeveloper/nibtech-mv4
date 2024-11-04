a88e50326bd11bc53950baf5b195bc733ade1e7a
haleem24 <haleem.shah24@gmail.com>
2024-11-04 19:34:30 +0000

    Sub Grade

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -330,7 +330,6 @@
                 mywindow.document.write('<html><head><title>' + document.title  + '</title>');
                 mywindow.document.write(`<link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" type="text/css" />`);
                 mywindow.document.write('</head><body >');
-                mywindow.document.write('<h1>' + document.title  + '</h1>');
                 mywindow.document.write(document.getElementById(elem).innerHTML);
                 mywindow.document.write('</body></html>');
 
