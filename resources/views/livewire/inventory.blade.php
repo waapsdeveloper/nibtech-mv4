18d89eb41139f38d05e600869f3718881d2093c7
haleem24 <haleem.shah24@gmail.com>
2024-11-04 19:09:25 +0000

    Sub Grade

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -321,7 +321,31 @@
 
 
         @if (session('user')->hasPermission('view_inventory_summery') && request('summery') && request('summery') == 1)
-        <div class="card">
+        <script>
+            function PrintElem(elem)
+{
+                var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+                mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+                mywindow.document.write('</head><body >');
+                mywindow.document.write('<h1>' + document.title  + '</h1>');
+                mywindow.document.write(document.getElementById(elem).innerHTML);
+                mywindow.document.write('</body></html>');
+
+                mywindow.document.close(); // necessary for IE >= 10
+                mywindow.focus(); // necessary for IE >= 10*/
+
+                mywindow.print();
+                mywindow.close();
+
+                return true;
+            }
+
+            $(document).ready(function(){
+                PrintElem('print_inv');
+            });
+        </script>
+        <div class="card" id="print_inv">
             <div class="card-header pb-0">
                 Available Stock Summery
             </div>
