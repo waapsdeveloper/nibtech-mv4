ebaef25dbba039e989d51760285bbe94e38d9760
haleem24 <haleem.shah24@gmail.com>
2024-11-04 19:32:29 +0000

    Sub Grade

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -328,7 +328,7 @@
                 var mywindow = window.open('', 'PRINT', 'height=400,width=600');
 
                 mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-                mywindow.document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" />');
+                mywindow.document.write(`<link rel="stylesheet" href="{{asset('assets/plugins/bootstrap/css/bootstrap.min.css')}}" type="text/css" />`);
                 mywindow.document.write('</head><body >');
                 mywindow.document.write('<h1>' + document.title  + '</h1>');
                 mywindow.document.write(document.getElementById(elem).innerHTML);
