60e9f2641855d3f2f93223dd868bf0b07e2b2f26
haleem24 <haleem.shah24@gmail.com>
2024-11-07 14:30:22 +0000

    Sub Grade

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -759,11 +759,13 @@
 
             $(document).ready(function(){
                 $('.select2').select2();
+                if(isset({{$stocks}})){
 
-                let total_stocks = {{ $stocks->total() }} ?? 0;
-                get_average_cost();
+                }
+                // let total_stocks = {{ $stocks->total() }} ?? 0;
+                // get_average_cost();
 
-                get_vendor_wise_average(total_stocks);
+                // get_vendor_wise_average(total_stocks);
 
             });
 
