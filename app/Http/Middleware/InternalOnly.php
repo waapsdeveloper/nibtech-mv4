f45b188bf0d58485117bfca8defddec1f58deb75
haleem24 <haleem.shah24@gmail.com>
2024-11-08 10:44:35 +0000

    Sub Grade

--- app/Http/Middleware/InternalOnly.php
+++ app/Http/Middleware/InternalOnly.php
@@ -23,7 +23,7 @@ class InternalOnly
 
         // Check if the request originated from the allowed domain
         if ($request->getHost() !== Env::get('APP_URL')) {
-            abort(401, 'Unauthorized access');
+            abort(401, 'Unauthorized accessu');
         }
 
         if (!in_array($request->ip(), $allowedIps)) {
