7ea83f044fa7c3e896d8c0b962efba392ba88460
haleem24 <haleem.shah24@gmail.com>
2024-11-11 20:05:50 +0000

    Correction Override

--- app/Exports/IMEILabelExport.php
+++ app/Exports/IMEILabelExport.php
@@ -99,8 +99,8 @@ class IMEILabelExport
         $pdf->SetFont('times', '', 9);
         $pdf->SetLineStyle(['width' => 0.1, 'color' => [0, 0, 0]]);
 
-        $pdf->MultiCell(20, 5, 'SO: '.$reference, 0, 'L', 0, 0, '', '', true, 0, false, true, 0, 'T');
-        $pdf->MultiCell(42, 5, 'Model: '.$model.' '.$storage.' '.$color.' '.$grade.' '.$sub_grade, 0, 'L', 0, 0, '', '', true, 0, false, true, 0, 'T');
+        $pdf->MultiCell(20, 5, 'SO: '.$reference, 0, 'L', 0, 0, '', '', true, 0, false, true, 0, 'T', true);
+        $pdf->MultiCell(42, 5, 'Model: '.$model.' '.$storage.' '.$color.' '.$grade.' '.$sub_grade, 0, 'L', 0, 0, '', '', true, 0, false, true, 0, 'T', true);
 
 
         $model = $variation->product->model;
