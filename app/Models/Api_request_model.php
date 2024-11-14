3837011a4b9f72dbbeb4030f57fd1fd5c84f934a
haleem24 <haleem.shah24@gmail.com>
2024-11-14 11:34:44 +0000

    Correction Override

--- app/Models/Api_request_model.php
+++ app/Models/Api_request_model.php
@@ -48,12 +48,12 @@ class Api_request_model extends Model
             // if (strpos($data, '{') !== false && strpos($data, '}') !== false) {
             //     $datas = preg_split('/(?<=\}),(?=\{)/', $data)[0];
             // }
-            // if (is_string($datas)) {
-            //     $datas = json_decode($datas);
-            // }
-            // if (is_string($datas)) {
-            //     $datas = json_decode($datas);
-            // }
+            if (is_string($datas)) {
+                $datas = json_decode($datas);
+            }
+            if (is_string($datas)) {
+                $datas = json_decode($datas);
+            }
             if($datas == null || ($datas->Imei == '' && $datas->Serial == '')){
                 continue;
             }
