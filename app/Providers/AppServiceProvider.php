bc1bfdabd06e421184f5a2970e1a6b4e8ced7c95
haleem24 <haleem.shah24@gmail.com>
2024-11-19 11:38:35 +0000

    Correction Override

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         $host = request()->getHost(); // Get the current domain
-        echo $envFile = match ($host) {
+        $envFile = match ($host) {
             'sdpos.nibritaintech.com' => '.env.sdpos',
             'egpos.nibritaintech.com' => '.env.egpos',
             default => '.env',
