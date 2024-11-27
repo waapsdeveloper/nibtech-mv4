4a614ce5bb369e1132de4ab548c3d84c3b326c4b
haleem24 <haleem.shah24@gmail.com>
2024-11-27 21:26:21 +0000

    Correction Override

--- resources/views/layouts/components/app-sidebar.blade.php
+++ resources/views/layouts/components/app-sidebar.blade.php
@@ -9,7 +9,6 @@
 								<img src="{{asset('assets/img/brand/favicon-white1.png')}}" class="main-logo  mobile-dark" alt="logo">
 							</a>
 						</div>
-
 						<div class="main-sidemenu">
 							<div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"/></svg></div>
 							<ul class="side-menu">
