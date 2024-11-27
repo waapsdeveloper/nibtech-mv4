2f51d28203f9751a354c068fc152ce0ee9c1ef0e
haleem24 <haleem.shah24@gmail.com>
2024-11-27 21:05:29 +0000

    Correction Override

--- resources/views/layouts/components/app-header.blade.php
+++ resources/views/layouts/components/app-header.blade.php
@@ -18,7 +18,7 @@
             </div>
             <div class="logo-horizontal">
                 <a href="{{url('index')}}" class="header-logo">
-                    <img src="{{asset('assets/img/brand/logo1.png')}}" class="mobile-logo logo-1" alt="logo"  >
+                    <img src="{{asset('assets/img/brand').'/'.session('app_logo')}}" class="mobile-logo logo-1" alt="logo"  >
                     <img src="{{asset('assets/img/brand/logo-white1.png')}}" class="mobile-logo dark-logo-1" alt="logo"  >
                 </a>
             </div>
