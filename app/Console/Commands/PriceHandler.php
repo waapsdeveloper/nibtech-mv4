21b30eeaf0e02949e5f801aef6850cb4fde8c0dc
haleem24 <haleem.shah24@gmail.com>
2024-11-29 13:17:34 +0000

    Price Handler

--- app/Console/Commands/PriceHandler.php
+++ app/Console/Commands/PriceHandler.php
@@ -103,6 +103,7 @@ class PriceHandler extends Command
                     $error .= json_encode($list);
                     continue;
                 }
+                echo $list->product_id;
                 $country = Country_model::where('code',$list->market)->first();
                 $listing = Listing_model::firstOrNew(['variation_id'=>$variation->id, 'country'=>$country->id]);
                 $listing->reference_uuid = $list->product_id;
