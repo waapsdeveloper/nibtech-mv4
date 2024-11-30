6133ab442481d4585779598d61c4faa3b3718b30
haleem24 <haleem.shah24@gmail.com>
2024-11-30 18:36:51 +0000

    Price Handler

--- app/Console/Commands/RefreshOrders.php
+++ app/Console/Commands/RefreshOrders.php
@@ -40,51 +40,6 @@ class RefreshOrders extends Command
 
     public function handle()
     {
-        $domains = DB::connection('master')->table('domains')->get();
-
-        foreach ($domains as $domain) {
-            // Dynamically update database connection
-            Config::set('database.connections.mysql.host', $domain->db_host);
-            Config::set('database.connections.mysql.port', $domain->db_port);
-            Config::set('database.connections.mysql.database', $domain->db_name);
-            Config::set('database.connections.mysql.username', $domain->db_username);
-            Config::set('database.connections.mysql.password', $domain->db_password);
-            // App Configuration
-            Config::set('app.url', 'https://' . $domain->domain);
-            Config::set('app.name', $domain->app_name);
-            Config::set('app.logo', $domain->app_logo);
-            Config::set('app.status', $domain->app_status);
-
-            // SMTP Configuration
-            Config::set('mail.mailer', 'smtp');
-            Config::set('mail.host', $domain->smtp_host);
-            Config::set('mail.port', $domain->smtp_port);
-            Config::set('mail.username', $domain->smtp_username);
-            Config::set('mail.password', $domain->smtp_password);
-            Config::set('mail.encryption', $domain->smtp_encryption);
-
-            // Backmarket API Configuration
-            Config::set('backmarket.api_key_1', $domain->backmarket_api_key_1);
-            Config::set('backmarket.api_key_2', $domain->backmarket_api_key_2);
-
-
-            DB::purge('mysql'); // Clear cached database connection
-            DB::reconnect('mysql'); // Reconnect to the updated database
-
-            // if($domain->domain == 'sdpos.nibritaintech.com'){
-            //     continue;
-            // }
-            $this->info("Running cron for domain: {$domain->domain}");
-
-            // Execute tenant-specific logic
-            $this->runTenantSpecificJobs($domain->domain);
-        }
-
-        $this->info('Tenant cron completed for all domains.');
-    }
-    public function runTenantSpecificJobs($domain)
-    {
-
         $bm = new BackMarketAPIController();
         $order_model = new Order_model();
         $order_item_model = new Order_item_model();
@@ -104,11 +59,7 @@ class RefreshOrders extends Command
         }
         echo 2;
 
-        if($domain == 'egpos.nibritaintech.com'){
-            $modification = '2024-03-01+00:00:00';
-        } else {
             $modification = false;
-        }
         $resArray = $bm->getAllOrders(1, ['page-size'=>50], $modification);
         if ($resArray !== null) {
             // print_r($resArray);
