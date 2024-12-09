a0aabea993943d8ce91ba4a12c5d8b956e88f7a0
haleem24 <haleem.shah24@gmail.com>
2024-12-09 19:59:06 +0000

    Price Handler

--- resources/views/livewire/vendor_report_new.blade.php
+++ resources/views/livewire/vendor_report_new.blade.php
@@ -267,31 +267,8 @@
                     { data: 'stock_cost' },
                 ],
                 paging: false,
-                // Row where Column 2 = Total at the end
-                "footerCallback": function ( row, data, start, end, display ) {
-                    var api = this.api(), data;
 
-                    // Remove the formatting to get integer data for summation
-                    var intVal = function ( i ) {
-                        return typeof i === 'string' ?
-                            i.replace(/[\€,]/g, '')*1 :
-                            typeof i === 'number' ?
-                                i : 0;
-                    };
 
-                    // Total over all pages
-                    total = api
-                        .column( 3 )
-                        .data()
-                        .reduce( function (a, b) {
-                            return intVal(a) + intVal(b);
-                        }, 0 );
-
-                    // Update footer
-                    $( api.column( 3 ).footer() ).html(
-                        '€'+total
-                    );
-                }
             });
 
             $('#rma_report').DataTable({
