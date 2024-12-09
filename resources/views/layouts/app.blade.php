89bf405a5ce8910839612f6e3d18156d3892bf8a
haleem24 <haleem.shah24@gmail.com>
2024-12-09 19:18:43 +0000

    Price Handler

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @php
             $session_url = session('url');
         @endphp
-		<title> {{ $title_page ?? null }} Britain Tech – Admin Panel </title>
+		<title> {{ session('title_page') ?? null }} Britain Tech – Admin Panel </title>
 
 
         @include('layouts.components.styles')
