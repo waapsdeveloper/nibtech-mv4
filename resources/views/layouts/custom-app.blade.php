cd287b74875d61947e225ce8ed2a15ef70fffd7c
haleem24 <haleem.shah24@gmail.com>
2025-01-12 16:36:29 +0000

    Repair Customer

--- resources/views/layouts/custom-app.blade.php
+++ resources/views/layouts/custom-app.blade.php
@@ -14,7 +14,7 @@
 
 		@include('layouts.components.custom-styles')
         @php
-            $primary_bg_color = env('PRIMARY_BG_COLOR') ?? '#052468';
+            $primary_bg_color = env('PRIMARY_BG_COLOR', '#052468');
         @endphp
 
         <script>
