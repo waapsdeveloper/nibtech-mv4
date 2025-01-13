553b70dc17757c71c2f90bf6644e69959be4b7ef
haleem24 <haleem.shah24@gmail.com>
2025-01-13 13:45:53 +0000

    Repair Customer

--- app/Exports/RepairsheetExport.php
+++ app/Exports/RepairsheetExport.php
@@ -73,6 +73,15 @@ class RepairsheetExport implements FromCollection, WithHeadings
             ->whereNull('process_stock.deleted_at')
             ->whereNull('stock.deleted_at')
             ->whereNull('p_stock.deleted_at')
+            ->whereNull('orders.deleted_at')
+            ->whereNull('customer.deleted_at')
+            ->whereNull('variation.deleted_at')
+            ->whereNull('products.deleted_at')
+            ->whereNull('color.deleted_at')
+            ->whereNull('storage.deleted_at')
+            ->whereNull('grade.deleted_at')
+            ->whereNull('order_items.deleted_at')
+            ->whereNull('stock_operations.deleted_at')
             ->orderBy('products.model', 'ASC')
             ->get();
 
