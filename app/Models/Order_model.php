c4dbf76ea5e20f2ca55d7c8e0681a84849472401
haleem24 <haleem.shah24@gmail.com>
2025-02-03 12:33:21 +0000

    Repair Customer

--- app/Models/Order_model.php
+++ app/Models/Order_model.php
@@ -113,6 +113,9 @@ class Order_model extends Model
                 $order->customer_id = $customer_model->updateCustomerInDB($orderObj, false, $currency_codes, $country_codes);
             }
             $order->status = $this->mapStateToStatus($orderObj);
+            if($order->status == null){
+                Log::info("Order status is null", $orderObj);
+            }
             $order->currency = $currency_codes[$orderObj->currency];
             $order->order_type_id = 3;
             $order->price = $orderObj->price;
