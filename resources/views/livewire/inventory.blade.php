4463a89373b7c5b88b0de10fb9d44d6bfe7926ca
haleem24 <haleem.shah24@gmail.com>
2025-02-05 22:04:52 +0000

    Repair Customer

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -460,7 +460,7 @@
                                             <td>{{ $i + 1 }}</td>
                                             <td>{{ $item->stock->variation->product->model ?? "Variation Model Not added"}} {{$storages[$item->stock->variation->storage] ?? null}} {{$colors[$item->stock->variation->color] ?? null}} {{$grades[$item->stock->variation->grade] ?? "Variation Grade Not added Reference: ".$item->stock->variation->reference_id }}</td>
                                             <td>{{ $item->stock->imei.$item->stock->serial_number }}</td>
-                                            <td>{{ $item->stock->order->customer->first_name ?? "Purchase Entry Error" }}</td>
+                                            <td>{{ $item->stock->order->customer->last_name ?? "Purchase Entry Error" }}</td>
                                             <td style="width:220px">{{ $item->created_at }}</td>
                                         </tr>
                                         @php
@@ -549,7 +549,7 @@
                                             <td><a title="Filter this variation" href="{{url('inventory').'?product='.$stock->variation->product_id.'&storage='.$stock->variation->storage.'&grade[]='.$stock->variation->grade}}">{{ (isset($stock->variation->product_id) ? $stock->variation->product->model . " " : null). (isset($stock->variation->storage) ? $storages[$stock->variation->storage] . " " : null) . " " .
                                             (isset($stock->variation->color) ? $colors[$stock->variation->color] . " " : null) . ($grades[$stock->variation->grade] ?? "Grade Issue") . (isset($stock->variation->sub_grade) ? " ".$grades[$stock->variation->sub_grade] : null) }} </a></td>
                                             <td><a title="{{$stock->id}} | Search Serial" href="{{url('imei')."?imei=".$stock->imei.$stock->serial_number}}" target="_blank"> {{$stock->imei.$stock->serial_number }} </a></td>
-                                            <td><a title="Vendor Profile" href="{{url('edit-customer').'/'.$stock->order->customer_id}}" target="_blank"> {{ $stock->order->customer->first_name ?? null}} </a></td>
+                                            <td><a title="Vendor Profile" href="{{url('edit-customer').'/'.$stock->order->customer_id}}" target="_blank"> {{ $stock->order->customer->last_name ?? null}} </a></td>
                                             <td>
                                                 <a title="Purchase Order Details" href="{{url('purchase/detail').'/'.$stock->order_id}}?status=1" target="_blank"> {{ $stock->order->reference_id }} </a>
                                                 @if ($stock->latest_return)
