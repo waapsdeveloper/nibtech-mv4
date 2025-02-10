644ceb6f4e0807a18a2d9f7fc7b36685da298b96
haleem24 <haleem.shah24@gmail.com>
2025-02-10 20:30:54 +0000

    Repair Customer

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -8,6 +8,20 @@ class OrdersExport
 {
     public function generatePdf()
     {
+        if(request('start_date') != '' && request('start_time') != ''){
+            $start_date = request('start_date').' '.request('start_time');
+        }elseif(request('start_date') != ''){
+            $start_date = request('start_date');
+        }else{
+            $start_date = 0;
+        }
+        if(request('end_date') != '' && request('end_time') != ''){
+            $end_date = request('end_date').' '.request('end_time');
+        }elseif(request('end_date') != ''){
+            $end_date = request('end_date')." 23:59:59";
+        }else{
+            $end_date = now();
+        }
         // Fetch data from the database
         $data = DB::table('orders')
             ->join('order_items', 'orders.id', '=', 'order_items.order_id')
@@ -27,11 +41,11 @@ class OrdersExport
             )
             ->where('orders.deleted_at', null)
             ->where('orders.order_type_id', 3)
-            ->when(request('start_date') != '', function ($q) {
-                return $q->where('orders.created_at', '>=', request('start_date', 0));
+            ->when(request('start_date') != '', function ($q) use ($start_date) {
+                return $q->where('orders.created_at', '>=', $start_date);
             })
-            ->when(request('end_date') != '', function ($q) {
-                return $q->where('orders.created_at', '<=', request('end_date', 0) . " 23:59:59");
+            ->when(request('end_date') != '', function ($q) use ($end_date) {
+                return $q->where('orders.created_at', '<=', $end_date);
             })
             ->when(request('status') != '', function ($q) {
                 return $q->where('orders.status', request('status'));
