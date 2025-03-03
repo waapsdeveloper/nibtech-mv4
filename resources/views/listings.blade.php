c2f0cc503bca3f42a03aa5f5fe356caf02c0bec4
haleem24 <haleem.shah24@gmail.com>
2025-03-03 12:24:50 +0000

    Repair Customer

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -757,8 +757,8 @@
                                                 <thead>
                                                     <tr>
                                                         <th width="80"><small><b>Country</b></small></th>
-                                                        <th width="100"><small><b>Min Handler</b></small></th>
-                                                        <th width="100"><small><b>Price Handler</b></small></th>
+                                                        <th width="100"><small><b>Min Hndlr</b></small></th>
+                                                        <th width="100"><small><b>Price Hndlr</b></small></th>
                                                         <th width="80"><small><b>BuyBox</b></small></th>
                                                         <th title="Min Price" width="130"><small><b>Min </b>(<b id="best_price_${variation.id}"></b>)</small></th>
                                                         <th width="120"><small><b>Price</b></small></th>
