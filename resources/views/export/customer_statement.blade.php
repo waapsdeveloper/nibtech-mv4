5e8c108d319891142dc46117e71d135cc76188dd
haleem24 <haleem.shah24@gmail.com>
2025-03-11 13:26:28 +0000

    Repair Customer

--- resources/views/export/customer_statement.blade.php
+++ resources/views/export/customer_statement.blade.php
@@ -103,8 +103,8 @@
                         <h5>Belfast, BT2 8LA</h5>
                         <h5>invoice@nibritaintech.com</h5>
                 </td>
-                <td width="200"></td>
-                <td width="150" style="text-align: right; padding:0; margin:0; line-height:10px">
+                <td width="210"></td>
+                <td width="140" style="text-align: right; padding:0; margin:0; line-height:10px">
                     <br>
                     <br>
                     <br>
