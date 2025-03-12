72671c9f4b07c943fff5b08bb514dcaf389cd439
haleem24 <haleem.shah24@gmail.com>
2025-03-12 14:47:24 +0000

    Repair Customer

--- resources/views/livewire/customer-profile.blade.php
+++ resources/views/livewire/customer-profile.blade.php
@@ -441,12 +441,12 @@
                         @csrf
                         <input type="hidden" name="customer_id" value="{{ $customer->id }}">
                         <input type="hidden" name="type" value="1">
-                        <input type="text" name="transaction_id" id="transaction_id">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel2">Record Payment for {{ $customer->company }}</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">&times;</button>
                         </div>
                         <div class="modal-body pd-20">
+                        <input type="hidden" name="transaction_id" id="transaction_id">
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
