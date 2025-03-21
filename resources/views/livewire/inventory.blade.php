9d25e7cb6a45d2237196af69de8c6a2a2b07e5d6
haleem24 <haleem.shah24@gmail.com>
2025-03-21 12:54:21 +0000

    Sales Report

--- resources/views/livewire/inventory.blade.php
+++ resources/views/livewire/inventory.blade.php
@@ -285,7 +285,10 @@
             <div class="">
                 @if ($active_inventory_verification == null)
                 <a class="btn btn-sm btn-secondary pd-x-20 " href="{{url('inventory/start_verification')}}">Start Inventory Verification</a>
-                <a class="btn btn-sm btn-secondary pd-x-20 " href="{{url('inventory/resume_verification')}}">Resume Inventory Verification</a>
+                @if ($last_verification_date > now()->subDays(10))
+
+                    <a class="btn btn-sm btn-secondary pd-x-20 " href="{{url('inventory/resume_verification')}}">Resume Inventory Verification</a>
+                @endif
 
                 <button class="btn btn-sm btn-secondary pd-x-20 " type="submit" form="export" name="inventorysheet" value="1">Export Sheet</button>
 
