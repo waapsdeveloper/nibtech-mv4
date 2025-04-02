b42969938cd5c7fc48e8d7daa86ddea7bfad1b62
haleem24 <haleem.shah24@gmail.com>
2025-04-02 09:12:45 +0100

    Permission

--- app/Models/Order_model.php
+++ app/Models/Order_model.php
@@ -57,7 +57,7 @@ class Order_model extends Model
                         $transaction->reference_id = $latest_transaction_ref+1;
                         $transaction->status = 1;
                         $transaction->save();
-                        $message .= "Transaction sales merged for order ".$this->reference_id." and transaction ".$transaction->reference_id;
+                        // $message .= "Transaction sales merged for order ".$this->reference_id." and transaction ".$transaction->reference_id;
                     }elseif($charge_name == $description){
                         $amount = $transaction->amount;
                         if($amount < 0){
@@ -73,7 +73,7 @@ class Order_model extends Model
                         $message .= "Transaction charge merged for order ".$this->reference_id." and transaction ".$transaction->reference_id;
                         $add = true;
                     }else{
-                        $message .= "Transaction charge not merged for order ".$this->reference_id." and transaction ".$description. " with charge ".$charge_name;
+                        // $message .= "Transaction charge not merged for order ".$this->reference_id." and transaction ".$description. " with charge ".$charge_name;
                     }
                 }
 
