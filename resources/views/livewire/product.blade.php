b2806c891f61f31d14b81da16de3155c81c47cc5
haleem24 <haleem.shah24@gmail.com>
2025-04-12 13:35:03 +0100

    Topup

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -236,7 +236,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="">Brand</label>
-                                <select class="form-select" placeholder="Input Brand" name="product[brand]" required>
+                                <select class="form-select" placeholder="Input Brand" name="product[brand]">
                                     <option value="">Select Brand</option>
                                     @foreach ($brands as $brand)
                                         <option value="{{ $brand->id }}">{{ $brand->name }}</option>
