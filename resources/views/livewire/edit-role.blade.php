ed4aadb251cb0484acfc779c8d2f346fa9efba60
haleem24 <haleem.shah24@gmail.com>
2025-04-12 11:01:54 +0100

    Topup

---
+++ resources/views/livewire/edit-role.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+    @section('styles')
+
+		<!--- Internal Select2 css-->
+		<link href="{{asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+
+    @endsection
+
+    @section('content')
+
+        <!-- breadcrumb -->
+        <div class="breadcrumb-header justify-content-between">
+            <div class="left-content">
+                <span class="main-content-title mg-b-0 mg-b-lg-1">Role Profile</span>
+            </div>
+            <div class="justify-content-center mt-2">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item tx-15"><a href="{{url('role')}}">Role</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Role Profile</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /breadcrumb -->
+        <hr style="border-bottom: 1px solid #000">
+        <!-- row -->
+        <div class="card">
+            <div class="card-body">
+                <form action="{{url('update-role')}}/{{$role->id}}" method="POST">
+                    @csrf
+                    <div class="row">
+
+                        <div class="col-md-4">
+                            <div class="row row-xs align-items-center mg-b-20">
+                                <div class="col-md-3">
+                                    <label class="form-label mg-b-0">Name</label>
+                                </div>
+                                <div class="col-md-9 mg-t-5 mg-md-t-0">
+                                    <input class="form-control" placeholder="Enter role's name" name="name" value="{{$role->name}}" type="text">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary pd-x-30 mg-r-5 mg-t-5" >Update</button>
+                </form>
+            </div>
+        </div>
+        <!-- /row -->
+
+
+        @endsection
+    @section('scripts')
+
+		<!-- Form-layouts js -->
+		<script src="{{asset('assets/js/form-layouts.js')}}"></script>
+
+		<!--Internal  Select2 js -->
+		<script src="{{asset('assets/plugins/select2/js/select2.min.js')}}"></script>
+
+    @endsection
