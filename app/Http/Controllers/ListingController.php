51e7b31f501f470d7daf18ce14e814b772d4ab17
haleem24 <haleem.shah24@gmail.com>
2025-04-15 17:43:51 +0100

    Topup

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -138,7 +138,8 @@ class ListingController extends Controller
             // $q->havingRaw('COUNT(*) < (listed_stock * 0.05)');
             // });
             // ->where('sku', null);
-            return $q->withCount('today_orders')->where('today_orders_count', '<', DB::raw('listed_stock * 0.05'));
+            return $q->withCount('today_orders');
+            // ->where('today_orders_count', '<', DB::raw('listed_stock * 0.05'));
         })
         ->when(request('handler_status') == 2, function ($q) {
             return $q->whereHas('listings', function ($q) {
@@ -183,6 +184,7 @@ class ListingController extends Controller
         })
         ->paginate($per_page)
         ->appends(request()->except('page'));
+
     }
 
     public function get_target_variations(){
