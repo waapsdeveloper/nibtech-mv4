ced20690ab83a63a8fcce83fa30782210099a787
haleem24 <haleem.shah24@gmail.com>
2025-04-29 15:51:49 +0100

    Topup

--- public/assets/js/exported.js
+++ public/assets/js/exported.js
@@ -567,7 +567,7 @@
         curve: 'smooth',
         show: true,
         width: 4,
-        colors: ["#C5102F", "#05255F"]
+        colors: ["#008000", "#C5102F", "#05255F"]
       },
       legend: {
         show: true,
