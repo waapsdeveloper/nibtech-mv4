0a3a60874e4a63d68cc9a1e7adcbf74ec3a43fd9
haleem24 <haleem.shah24@gmail.com>
2025-05-16 16:50:39 +0100

    Purchase Export

--- app/Http/Livewire/Topup.php
+++ app/Http/Livewire/Topup.php
@@ -490,7 +490,7 @@ class Topup extends Component
         $listingController = new ListingController();
         foreach($listed_stocks as $listed_stock){
             if($listed_stock->variation_id != null){
-                $listingController->add_quantity($listed_stock->variation_id, -$listed_stock->qty_change);
+                $listingController->add_quantity($listed_stock->variation_id, -$listed_stock->qty_change, $id);
             }
         }
         session()->put('success', 'Topup Undoned');
