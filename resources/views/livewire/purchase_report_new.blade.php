3f000324218f33962c2db2f20398f72d3efe6fff
haleem24 <haleem.shah24@gmail.com>
2025-06-03 15:47:18 +0100

    Purchase Export

--- resources/views/livewire/purchase_report_new.blade.php
+++ resources/views/livewire/purchase_report_new.blade.php
@@ -15,15 +15,12 @@
                     <h4 class="card-title mb-0">Purchase Report</h4>
 
                     <form action="" method="GET" id="index" class="mb-0 form-inline">
-                        <div class="form-floating">
-                            <select class="form-select" name="vendor" id="vendor" class="wd-150">
-                                <option value="">All Vendors</option>
-                                @foreach ($vendors as $vendor_id => $vendor_name)
-                                    <option value="{{ $vendor_id }}" {{ $vendor_id == request('vendor') ? 'selected' : '' }}>{{ $vendor_name }}</option>
-                                @endforeach
-                            </select>
-                            <label for="vendor_id">Vendor</label>
-                        </div>
+                        <select class="form-select" name="vendor" id="vendor" class="wd-150">
+                            <option value="">All Vendors</option>
+                            @foreach ($vendors as $vendor_id => $vendor_name)
+                                <option value="{{ $vendor_id }}" {{ $vendor_id == request('vendor') ? 'selected' : '' }}>{{ $vendor_name }}</option>
+                            @endforeach
+                        </select>
                         <div class="form-floating">
                             <input class="form-control" id="datetimepicker" type="date" id="start" name="start_date" value="{{$start_date}}">
                             <label for="start">{{ __('locale.Start Date') }}</label>
