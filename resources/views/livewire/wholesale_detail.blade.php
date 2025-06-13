0a72b6607da6c1438c06a5d043d6c4713ba1a735
haleem24 <haleem.shah24@gmail.com>
2025-06-13 17:32:18 +0100

    DRFones New API

--- resources/views/livewire/wholesale_detail.blade.php
+++ resources/views/livewire/wholesale_detail.blade.php
@@ -510,21 +510,10 @@
 
                                 // If there are a lot of item_ids, use FormData for better handling
                                 if (form.find('input[name="item_ids[]"]').length > 500) {
-                                    var fd = new FormData(form[0]);
-                                    $.ajax({
-                                        type: "POST",
-                                        url: actionUrl,
-                                        data: fd,
-                                        processData: false,
-                                        contentType: false,
-                                        success: function(data) {
-                                            alert("Success: " + data);
-                                            $('#unit_price_' + id).addClass('bg-lightgreen');
-                                        },
-                                        error: function(jqXHR, textStatus, errorThrown) {
-                                            alert("Error: " + textStatus + " - " + errorThrown);
-                                        }
-                                    });
+                                    // Submit via normal form POST if over 500 items
+                                    form.off('submit'); // Remove this handler to avoid recursion
+                                    form[0].submit();
+                                    return;
                                 } else {
                                     $.ajax({
                                         type: "POST",
