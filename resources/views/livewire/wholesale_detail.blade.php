41b98dc0a2757e0049165c89cb345f3d519558c0
haleem24 <haleem.shah24@gmail.com>
2025-06-13 17:16:57 +0100

    DRFones New API

--- resources/views/livewire/wholesale_detail.blade.php
+++ resources/views/livewire/wholesale_detail.blade.php
@@ -512,7 +512,7 @@
                                     url: actionUrl,
                                     data: form.serialize(), // serializes the form's elements.
                                     success: function(data) {
-                                        // alert("Success: " + data); // show response from the PHP script.
+                                        alert("Success: " + data); // show response from the PHP script.
                                         $('#unit_price_' + id).addClass('bg-lightgreen');
                                     },
                                     error: function(jqXHR, textStatus, errorThrown) {
