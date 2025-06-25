39c0ff5618a2e266d0e1904ab650bee681f633cd
haleem24 <haleem.shah24@gmail.com>
2025-06-25 14:44:55 +0100

    s

--- resources/views/livewire/belfast_inventory.blade.php
+++ resources/views/livewire/belfast_inventory.blade.php
@@ -470,6 +470,27 @@
                 mywindow.document.write(
                     `<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css" />`);
                 mywindow.document.write('<title>' + document.title + '</title></head><body>');
+                mywindow.document.write(`
+                    <style>
+                        @media print {
+                            body, html {
+                                width: 100% !important;
+                                margin: 0;
+                                padding: 0;
+                            }
+                            .table {
+                                width: 100% !important;
+                                max-width: 100% !important;
+                                table-layout: auto !important;
+                                word-break: break-word;
+                            }
+                            .card, .card-body, .card-header {
+                                width: 100% !important;
+                                max-width: 100% !important;
+                            }
+                        }
+                    </style>
+                `);
                 mywindow.document.write(content);
                 mywindow.document.write('</body></html>');
 
