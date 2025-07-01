b0b9e93c118a296ff07368425b60b1435fe805ce
haleem24 <haleem.shah24@gmail.com>
2025-07-01 17:22:13 +0100

    Try2

--- resources/views/livewire/wholesale_detail.blade.php
+++ resources/views/livewire/wholesale_detail.blade.php
@@ -154,7 +154,7 @@
                     <button class="btn btn-sm btn-primary" data-bs-toggle="collapse" data-bs-target="#sheet-upload-form" aria-expanded="false" aria-controls="sheet-upload-form">Upload Sheet</button>
                     <button class="btn btn-sm btn-primary" data-bs-toggle="collapse" data-bs-target="#wholesale_charges" aria-expanded="false" aria-controls="wholesale_charges">Add Charge</button>
 
-                    <span class="form-check form-switch ms-4" title="Bypass Wholesale check" onclick="$('#bypass_check').check()">
+                    <span class="form-check form-switch ms-4 p-1" title="Bypass Wholesale check" onclick="$('#bypass_check').check()">
                         <input type="checkbox" value="1" id="bypass_check" name="bypass_check" class="form-check-input" form="wholesale_item" @if (session('bypass_check') == 1) checked @endif>
                         <label class="form-check-label" for="bypass_check">Bypass check</label>
                     </span>
