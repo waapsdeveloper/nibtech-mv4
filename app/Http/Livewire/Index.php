0042aa2239a6bd17dc2a5183173255763b6a72ba
haleem24 <haleem.shah24@gmail.com>
2025-07-03 15:02:01 +0100

    Print

--- app/Http/Livewire/Index.php
+++ app/Http/Livewire/Index.php
@@ -416,7 +416,7 @@ class Index extends Component
             $vendor_names = Customer_model::whereIn('id', $orders)->pluck('last_name','id')->toArray();
             $data = [];
             foreach($orders as $key => $order){
-                $data[] = '<a href="'. url('move_inventory') . '?po=' . $key . '/' . $vendor_names[$order] .'" title="view">'.$key.'('.$vendor_names[$order].')</a>';
+                $data[] = '<a href="'. url('move_inventory') . '?po=' . $key . '/' . $vendor_names[$order] .'" title="view" target="_blank">'.$key.'('.$vendor_names[$order].')</a>';
             }
             return response()->json($data);
         }else{
@@ -465,7 +465,7 @@ class Index extends Component
 
             $data['output'] = [];
             foreach($data['pss'] as $item){
-                $data['output'][] = '<a href="'. url('move_inventory') . '?pss=' . $item['id'] . '" title="view">'.$item['name'] . '(' . $item['total_stocks'] . ')</a> &nbsp;';
+                $data['output'][] = '<a href="'. url('move_inventory') . '?pss=' . $item['id'] . '" title="view" target="_blank">'.$item['name'] . '(' . $item['total_stocks'] . ')</a> &nbsp;';
             }
 
             return response()->json($data);
