b37bc1c8258270a19307e716200c572763a48a8a
haleem24 <haleem.shah24@gmail.com>
2025-11-20 13:27:48 +0000

    readd all git

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -618,7 +618,7 @@ class ListingController extends Controller
                 // Log::error($error);
                 return response()->json(['error'=>$error]);
             }
-            Log::info("Responses for variation ID $id: " . json_encode($responses));
+            // Log::info("Responses for variation ID $id: " . json_encode($responses));
             foreach($responses as $list){
                 if(is_string($list) || is_int($list)){
                     $error .= $list;
@@ -743,7 +743,7 @@ class ListingController extends Controller
         $listing->save();
         // print_r($response);
         // die;
-        // if($listing->country_id->code == 'GB'){
+        // if($listing->country_id->code == 'SE'){
         //     Log::info("Updated listing price for listing ID $id: " . json_encode($response));
         // }
         if(request('min_price')){
