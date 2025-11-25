3e13c76082e947db9665e0b66a9f8550987d9e75
haleem24 <haleem.shah24@gmail.com>
2025-11-25 13:03:39 +0000

    Refurbed

--- resources/views/livewire/order.blade.php
+++ resources/views/livewire/order.blade.php
@@ -662,7 +662,7 @@
                                         @isset($order->processed_by) | {{ $admins[$order->processed_by][0] }} | @endisset
                                         @isset($stock->tester) ({{ $stock->tester }}) @endisset
 
-                                        @if ((request('invoice') || request('packing')) && isset($stock) && $item->status == 2 && !session()->has('refresh'))
+                                        @if (isset($stock) && $item->status == 2 && !session()->has('refresh'))
                                             @php
                                                 session()->put('refresh', true);
                                             @endphp
