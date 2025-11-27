7e0b51043a051ddec64434037d20e3412a2abadb
haleem24 <haleem.shah24@gmail.com>
2025-11-27 19:07:44 +0000

    Refurbed

--- resources/views/livewire/order.blade.php
+++ resources/views/livewire/order.blade.php
@@ -690,7 +690,7 @@
                                         @isset($order->processed_by) | {{ $admins[$order->processed_by][0] }} | @endisset
                                         @isset($stock->tester) ({{ $stock->tester }}) @endisset
 
-                                        @if (isset($stock) && $item->status == 2 && !session()->has('refresh') && $order->marketplace_id == 1)
+                                        @if (isset($stock) && $item->status == 2 && !session()->has('refresh') && $order->marketplace_id != 4)
                                             @php
                                                 session()->put('refresh', true);
                                             @endphp
