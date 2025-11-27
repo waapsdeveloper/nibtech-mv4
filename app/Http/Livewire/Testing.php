cf8a07e24e1b6bc69c9e94f831993b23b1efd9ed
haleem24 <haleem.shah24@gmail.com>
2025-11-27 22:17:58 +0000

    Refurbed

--- app/Http/Livewire/Testing.php
+++ app/Http/Livewire/Testing.php
@@ -39,7 +39,7 @@ class Testing extends Component
         // $this->remove_extra_variations();
 
         // die;
-        $requests = Api_request_model::whereNull('status')->limit(200)->get();
+        $requests = Api_request_model::whereNull('status')->limit(100)->get();
 
         $data['requests'] = $requests;
 
