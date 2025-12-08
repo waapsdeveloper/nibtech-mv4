1024d32fbb9b6699cd0518168704c8d42efd7f84
haleem24 <haleem.shah24@gmail.com>
2025-12-08 15:31:53 +0000

    Refurbed

--- app/Http/Livewire/ChatNotificationIcon.php
+++ app/Http/Livewire/ChatNotificationIcon.php
@@ -8,20 +8,43 @@ use Livewire\Component;
 class ChatNotificationIcon extends Component
 {
     public int $count = 0;
+    public int $lastCount = 0;
 
     protected $listeners = ['chatNotificationsUpdated' => 'refreshCount'];
 
     public function mount(): void
     {
-        $this->refreshCount();
+        $initial = $this->resolveUnreadCount();
+        $this->count = $initial;
+        $this->lastCount = $initial;
     }
 
     public function refreshCount(): void
     {
+        $newCount = $this->resolveUnreadCount();
+
+        if ($newCount > $this->lastCount) {
+            $this->dispatchBrowserEvent('chat-notification-tone', [
+                'count' => $newCount,
+                'delta' => $newCount - $this->lastCount,
+            ]);
+        }
+
+        $this->count = $newCount;
+        $this->lastCount = $newCount;
+    }
+
+    protected function resolveUnreadCount(): int
+    {
         $adminId = session('user_id');
-        $this->count = $adminId
-            ? ChatNotification::where('admin_id', $adminId)->whereNull('read_at')->count()
-            : 0;
+
+        if (! $adminId) {
+            return 0;
+        }
+
+        return ChatNotification::where('admin_id', $adminId)
+            ->whereNull('read_at')
+            ->count();
     }
 
     public function render()
