0d8f50c75bf7ce4606a7db3116f3b3c786fbf751
Development Team <dev@nibritaintech.local>
2025-12-18 00:52:00 +0500

    feat: Add Stock Locks menu item to V2 sidebar
    - Added Stock Locks menu item in V2 submenu
    - Links to v2/stock-locks dashboard
    - Includes lock icon for visual consistency
    - Protected with view_listing permission

--- resources/views/layouts/components/app-sidebar.blade.php
+++ resources/views/layouts/components/app-sidebar.blade.php
@@ -59,6 +59,14 @@
                                             <a class="side-menu__item ps-0" href="{{url('v2/marketplace/stock-formula')}}">Stock Formula</a>
                                         </li>
                                         @endif
+                                        @if ($user->hasPermission('view_listing'))
+                                        <li class="slide">
+                                            <a class="side-menu__item ps-0" href="{{url('v2/stock-locks')}}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 8px; vertical-align: middle;"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
+                                                Stock Locks
+                                            </a>
+                                        </li>
+                                        @endif
                                     </ul>
                                 </li>
                                 @endif
