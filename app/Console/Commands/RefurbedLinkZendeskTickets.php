46e83ae63fd32bdcc3d6f6eab0a3ce52679859cd
haleem24 <haleem.shah24@gmail.com>
2026-02-24 12:05:42 +0000

    Fix: Replace axios with api service in storefront store and install marked package

--- app/Console/Commands/RefurbedLinkZendeskTickets.php
+++ app/Console/Commands/RefurbedLinkZendeskTickets.php
@@ -78,7 +78,7 @@ class RefurbedLinkZendeskTickets extends BaseCommand
             }
         }
 
-        return Command::SUCCESS;
+        return self::SUCCESS;
     }
 
     protected function labelsOption(): ?array
